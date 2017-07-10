58214ce84566731a2570e1c505435d2d6373b802
Dean Wilson <dean.wilson@digital.cabinet-office.gov.uk>
2017-07-10 13:27:52 +0100

    Move the common commands up to the default user data.
    This allows per app roles to be smaller and more focused.

--- terraform/modules/aws/node_group/user_data.sh
+++ terraform/modules/aws/node_group/user_data.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 
-# Ubuntu
+# Source DISTRIB_* env vars
+. /etc/lsb-release
+
+export DEBIAN_FRONTEND=noninteractive
+echo "LC_ALL=en_GB.UTF-8" > /etc/environment
+locale-gen "en_GB.UTF-8"
+dpkg-reconfigure locales
+
+# regenerate the servers hosts keys to ensure they are unique
+rm -f /etc/ssh/ssh_host*key*
+dpkg-reconfigure openssh-server
+
+# Install puppet
+TMPFILE=$(mktemp)
+wget -qO ${TMPFILE} http://apt.puppetlabs.com/puppetlabs-release-${DISTRIB_CODENAME}.deb
+dpkg -i ${TMPFILE}
+rm ${TMPFILE}
+
+# Ensure the packages are upto date.
 apt-get update -qq
 apt-get -y upgrade
 ##############
@@ -24,6 +42,10 @@ done
 # services like puppet to resolve
 PROJECT=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE_ID" "Name=tag-key,Values=Project" --region=$REGION --output=json | jq -r '.Tags[] | .Value ')
 sed -r -i "s/^search(.*)$/search\1 ${PROJECT}.internal/g" /etc/resolv.conf
+
+# install puppet
+apt-get -y install make ruby1.9.3 puppet='3.8.*' puppet-common='3.8.*'
+service puppet stop
 
 # Append addition user-data script
 ${additional_user_data_script}
