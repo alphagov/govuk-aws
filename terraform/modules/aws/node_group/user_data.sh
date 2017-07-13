6eab8b0c70e94882d068f12c1f7bcb9f41998693
Ana Fernandez <ana.fernandez@digital.cabinet-office.gov.uk>
2017-07-13 12:46:37 +0100

    Update user_data dhclient and output
    Update the user_data script in the node_group module to configure dhclient
    to append the stack search domain.
    
    Update user_data scripts in node_group and the app-graphite project to output
    more information.

--- terraform/modules/aws/node_group/user_data.sh
+++ terraform/modules/aws/node_group/user_data.sh
@@ -1,30 +1,41 @@
 #!/usr/bin/env bash
 
+#
+# Snippet: base
+#
+
+echo "[$(date '+%H:%M:%S %d-%m-%Y')] START SNIPPET: base"
+
 # Source DISTRIB_* env vars
 . /etc/lsb-release
 
+echo "[$(date '+%H:%M:%S %d-%m-%Y')] base: configure locales ..."
 export DEBIAN_FRONTEND=noninteractive
 echo "LC_ALL=en_GB.UTF-8" > /etc/environment
 locale-gen "en_GB.UTF-8"
 dpkg-reconfigure locales
 
-# regenerate the servers hosts keys to ensure they are unique
+echo "[$(date '+%H:%M:%S %d-%m-%Y')] base: regenerate the servers hosts keys to ensure they are unique ..."
 rm -f /etc/ssh/ssh_host*key*
 dpkg-reconfigure openssh-server
 
-# Install puppet
+echo "[$(date '+%H:%M:%S %d-%m-%Y')] base: install Puppet repository ..."
 TMPFILE=$(mktemp)
 wget -qO $TMPFILE http://apt.puppetlabs.com/puppetlabs-release-$DISTRIB_CODENAME.deb
 dpkg -i $TMPFILE
 rm $TMPFILE
 
-# Ensure the packages are upto date.
+echo "[$(date '+%H:%M:%S %d-%m-%Y')] base: ensure the packages are up to date ..."
 apt-get update -qq
 apt-get -y upgrade
-##############
 
+echo "[$(date '+%H:%M:%S %d-%m-%Y')] base: install AWS cli ..."
 apt-get install -y awscli jq
 
+echo "[$(date '+%H:%M:%S %d-%m-%Y')] base: install LVM ..."
+apt-get install -y lvm2
+
+echo "[$(date '+%H:%M:%S %d-%m-%Y')] base: generate local facts ..."
 INSTANCE_ID=$(ec2metadata --instance-id)
 REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep region | awk -F\" '{print $4}')
 TAGS=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE_ID" "Name=tag-key,Values=aws_*" --region=$REGION --output=json | jq -r '.Tags[] | [.Key, .Value] | @csv' | tr -d '"')
@@ -38,14 +49,21 @@ do
   echo $KEY=$VALUE > /etc/facter/facts.d/$KEY.txt
 done
 
-# Add search domain for the local project. This allows local
-# services like puppet to resolve
-PROJECT=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE_ID" "Name=tag-key,Values=Project" --region=$REGION --output=json | jq -r '.Tags[] | .Value ')
-sed -r -i "s/^search(.*)$/search\1 $PROJECT.internal/g" /etc/resolv.conf
-
-# install puppet
+echo "[$(date '+%H:%M:%S %d-%m-%Y')] base: install Puppet ..."
 apt-get -y install make ruby1.9.3 puppet='3.8.*' puppet-common='3.8.*'
 service puppet stop
+
+echo "[$(date '+%H:%M:%S %d-%m-%Y')] base: add search domain for local stack ..."
+F_STACKNAME=$(facter aws_stackname)
+
+if [[ -z $F_STACKNAME ]] ; then
+  echo "ERROR aws_stackname is null"
+else
+  echo "append domain-search \""$F_STACKNAME".internal\";" >> /etc/dhcp/dhclient.conf
+  dhclient -r; dhclient
+fi
+
+echo "[$(date '+%H:%M:%S %d-%m-%Y')] END SNIPPET: base"
 
 # Append addition user-data script
 ${additional_user_data_script}
