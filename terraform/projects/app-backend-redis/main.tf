36f29053fc49ce15c0a3844d1faa5de8681b5369
Dean Wilson <dean.wilson@digital.cabinet-office.gov.uk>
2017-07-14 12:58:49 +0100

    Add redis-backend elasticache cluster
    This used to be the host called "redis-1.backend" so i've renamed
    the cluster to be redis-backend rather than end up with the even
    worse name of "redis"

---
+++ terraform/projects/app-backend-redis/main.tf
@@ -0,0 +1,144 @@
+# == Manifest: projects::app-backend-redis
+#
+# Backend VDC Redis Elasticache cluster
+#
+# === Variables:
+#
+# aws_region
+# remote_state_govuk_vpc_bucket
+# remote_state_govuk_vpc_key
+# stackname
+#
+# === Outputs:
+#
+
+variable "aws_region" {
+  type        = "string"
+  description = "AWS region"
+  default     = "eu-west-1"
+}
+
+variable "remote_state_govuk_vpc_key" {
+  type        = "string"
+  description = "VPC TF remote state key"
+}
+
+variable "remote_state_govuk_vpc_bucket" {
+  type        = "string"
+  description = "VPC TF remote state bucket"
+}
+
+variable "remote_state_govuk_networking_key" {
+  type        = "string"
+  description = "VPC TF remote state key"
+}
+
+variable "remote_state_govuk_networking_bucket" {
+  type        = "string"
+  description = "VPC TF remote state bucket"
+}
+
+variable "remote_state_govuk_security_groups_key" {
+  type        = "string"
+  description = "VPC TF remote state key"
+}
+
+variable "remote_state_govuk_security_groups_bucket" {
+  type        = "string"
+  description = "VPC TF remote state bucket"
+}
+
+variable "remote_state_govuk_internal_dns_zone_key" {
+  type        = "string"
+  description = "VPC TF remote state key"
+}
+
+variable "remote_state_govuk_internal_dns_zone_bucket" {
+  type        = "string"
+  description = "VPC TF remote state bucket"
+}
+
+variable "stackname" {
+  type        = "string"
+  description = "Stackname"
+}
+
+# Resources
+# --------------------------------------------------------------
+terraform {
+  backend          "s3"             {}
+  required_version = "= 0.9.10"
+}
+
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+data "terraform_remote_state" "govuk_vpc" {
+  backend = "s3"
+
+  config {
+    bucket = "${var.remote_state_govuk_vpc_bucket}"
+    key    = "${var.remote_state_govuk_vpc_key}"
+    region = "eu-west-1"
+  }
+}
+
+data "terraform_remote_state" "govuk_networking" {
+  backend = "s3"
+
+  config {
+    bucket = "${var.remote_state_govuk_networking_bucket}"
+    key    = "${var.remote_state_govuk_networking_key}"
+    region = "eu-west-1"
+  }
+}
+
+data "terraform_remote_state" "govuk_security_groups" {
+  backend = "s3"
+
+  config {
+    bucket = "${var.remote_state_govuk_security_groups_bucket}"
+    key    = "${var.remote_state_govuk_security_groups_key}"
+    region = "eu-west-1"
+  }
+}
+
+data "terraform_remote_state" "govuk_internal_dns_zone" {
+  backend = "s3"
+
+  config {
+    bucket = "${var.remote_state_govuk_internal_dns_zone_bucket}"
+    key    = "${var.remote_state_govuk_internal_dns_zone_key}"
+    region = "eu-west-1"
+  }
+}
+
+resource "aws_route53_record" "service_record" {
+  zone_id = "${data.terraform_remote_state.govuk_internal_dns_zone.internal_service_zone_id}"
+  name    = "backend-redis"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["${module.backend_redis_cluster.configuration_endpoint_address}"]
+}
+
+module "backend_redis_cluster" {
+  source             = "../../modules/aws/elasticache_redis_cluster"
+  name               = "${var.stackname}-backend-redis"
+  default_tags       = "${map("Project", var.stackname, "aws_stackname", var.stackname, "aws_migration", "backend-redis")}"
+  subnet_ids         = "${data.terraform_remote_state.govuk_networking.private_subnet_elasticache_ids}"
+  security_group_ids = ["${data.terraform_remote_state.govuk_security_groups.sg_backend-redis_id}"]
+}
+
+# Outputs
+# --------------------------------------------------------------
+
+output "backend_redis_configuration_endpoint_address" {
+  value       = "${module.backend_redis_cluster.configuration_endpoint_address}"
+  description = "Backend VDC redis configuration endpoint address"
+}
+
+output "service_dns_name" {
+  value       = "${aws_route53_record.service_record.fqdn}"
+  description = "DNS name to access the node service"
+}
