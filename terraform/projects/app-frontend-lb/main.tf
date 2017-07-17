a4b99323cb06fe6b81a8ea67102d0402c791953a
Dean Wilson <dean.wilson@digital.cabinet-office.gov.uk>
2017-07-17 16:59:54 +0100

    Use the new bucket names.
    Refactor govuk to infra in the infra projects

--- terraform/projects/app-frontend-lb/main.tf
+++ terraform/projects/app-frontend-lb/main.tf
@@ -5,8 +5,6 @@
 # === Variables:
 #
 # aws_region
-# remote_state_govuk_vpc_key
-# remote_state_govuk_vpc_bucket
 # stackname
 #
 # === Outputs:
@@ -18,44 +16,9 @@ variable "aws_region" {
   default     = "eu-west-1"
 }
 
-variable "remote_state_govuk_vpc_key" {
+variable "remote_state_bucket" {
   type        = "string"
-  description = "VPC TF remote state key"
-}
-
-variable "remote_state_govuk_vpc_bucket" {
-  type        = "string"
-  description = "VPC TF remote state bucket"
-}
-
-variable "remote_state_govuk_networking_key" {
-  type        = "string"
-  description = "VPC TF remote state key"
-}
-
-variable "remote_state_govuk_networking_bucket" {
-  type        = "string"
-  description = "VPC TF remote state bucket"
-}
-
-variable "remote_state_govuk_security_groups_key" {
-  type        = "string"
-  description = "VPC TF remote state key"
-}
-
-variable "remote_state_govuk_security_groups_bucket" {
-  type        = "string"
-  description = "VPC TF remote state bucket"
-}
-
-variable "remote_state_govuk_internal_dns_zone_key" {
-  type        = "string"
-  description = "VPC TF remote state key"
-}
-
-variable "remote_state_govuk_internal_dns_zone_bucket" {
-  type        = "string"
-  description = "VPC TF remote state bucket"
+  description = "S3 bucket we store our terraform state in"
 }
 
 variable "stackname" {
@@ -79,50 +42,50 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
-data "terraform_remote_state" "govuk_vpc" {
+data "terraform_remote_state" "infra_vpc" {
   backend = "s3"
 
   config {
-    bucket = "${var.remote_state_govuk_vpc_bucket}"
-    key    = "${var.remote_state_govuk_vpc_key}"
+    bucket = "${var.remote_state_bucket}"
+    key    = "${var.stackname}/infra-vpc.tfstate"
     region = "eu-west-1"
   }
 }
 
-data "terraform_remote_state" "govuk_networking" {
+data "terraform_remote_state" "infra_networking" {
   backend = "s3"
 
   config {
-    bucket = "${var.remote_state_govuk_networking_bucket}"
-    key    = "${var.remote_state_govuk_networking_key}"
+    bucket = "${var.remote_state_bucket}"
+    key    = "${var.stackname}/infra-networking.tfstate}"
     region = "eu-west-1"
   }
 }
 
-data "terraform_remote_state" "govuk_security_groups" {
+data "terraform_remote_state" "infra_security_groups" {
   backend = "s3"
 
   config {
-    bucket = "${var.remote_state_govuk_security_groups_bucket}"
-    key    = "${var.remote_state_govuk_security_groups_key}"
+    bucket = "${var.remote_state_bucket}"
+    key    = "${var.stackname}/infra-security-groups.tfstate"
     region = "eu-west-1"
   }
 }
 
-data "terraform_remote_state" "govuk_internal_dns_zone" {
+data "terraform_remote_state" "infra_internal_dns_zone" {
   backend = "s3"
 
   config {
-    bucket = "${var.remote_state_govuk_internal_dns_zone_bucket}"
-    key    = "${var.remote_state_govuk_internal_dns_zone_key}"
+    bucket = "${var.remote_state_bucket}"
+    key    = "${var.stackname}/infra-internal-dns-zone.tfstate"
     region = "eu-west-1"
   }
 }
 
 resource "aws_elb" "frontend-lb_elb" {
   name            = "${var.stackname}-frontend-lb"
-  subnets         = ["${data.terraform_remote_state.govuk_networking.private_subnet_ids}"]
-  security_groups = ["${data.terraform_remote_state.govuk_security_groups.sg_frontend-lb_elb_id}"]
+  subnets         = ["${data.terraform_remote_state.infra_networking.private_subnet_ids}"]
+  security_groups = ["${data.terraform_remote_state.infra_security_groups.sg_frontend-lb_elb_id}"]
   internal        = "true"
 
   listener {
@@ -150,7 +113,7 @@ resource "aws_elb" "frontend-lb_elb" {
 }
 
 resource "aws_route53_record" "frontend-lb_service_record" {
-  zone_id = "${data.terraform_remote_state.govuk_internal_dns_zone.internal_service_zone_id}"
+  zone_id = "${data.terraform_remote_state.infra_internal_dns_zone.internal_service_zone_id}"
   name    = "frontend-lb"
   type    = "A"
 
@@ -164,10 +127,10 @@ resource "aws_route53_record" "frontend-lb_service_record" {
 module "frontend-lb" {
   source                               = "../../modules/aws/node_group"
   name                                 = "${var.stackname}-frontend-lb"
-  vpc_id                               = "${data.terraform_remote_state.govuk_vpc.vpc_id}"
+  vpc_id                               = "${data.terraform_remote_state.infra_vpc.vpc_id}"
   default_tags                         = "${map("Project", var.stackname, "aws_stackname", var.stackname, "aws_migration", "frontend_lb", "aws_hostname", "frontend-lb-1")}"
-  instance_subnet_ids                  = "${data.terraform_remote_state.govuk_networking.private_subnet_ids}"
-  instance_security_group_ids          = ["${data.terraform_remote_state.govuk_security_groups.sg_frontend-lb_id}", "${data.terraform_remote_state.govuk_security_groups.sg_management_id}"]
+  instance_subnet_ids                  = "${data.terraform_remote_state.infra_networking.private_subnet_ids}"
+  instance_security_group_ids          = ["${data.terraform_remote_state.infra_security_groups.sg_frontend-lb_id}", "${data.terraform_remote_state.infra_security_groups.sg_management_id}"]
   instance_type                        = "t2.medium"
   create_instance_key                  = true
   instance_key_name                    = "${var.stackname}-frontend-lb"
