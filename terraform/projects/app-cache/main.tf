0545fe44512e599ef8fe4fa5de928222ae93d550
Ana Fernandez <ana.fernandez@digital.cabinet-office.gov.uk>
2017-07-20 11:53:54 +0100

    Refactor app-cache
    - Add user_data_snippets.tf and remove old additional_user_data.txt
    - Add aws_migration variable and tag
    - Use node_group use_data usage

--- terraform/projects/app-cache/main.tf
+++ terraform/projects/app-cache/main.tf
@@ -6,6 +6,8 @@
 #
 # aws_region
 # stackname
+# aws_environment
+# ssh_public_key
 #
 # === Outputs:
 #
@@ -19,6 +21,11 @@ variable "aws_region" {
 variable "stackname" {
   type        = "string"
   description = "Stackname"
+}
+
+variable "aws_environment" {
+  type        = "string"
+  description = "AWS Environment"
 }
 
 variable "ssh_public_key" {
@@ -64,7 +71,7 @@ resource "aws_elb" "cache_elb" {
   connection_draining         = true
   connection_draining_timeout = 400
 
-  tags = "${map("Name", "${var.stackname}-cache", "Project", var.stackname, "aws_migration", "cache")}"
+  tags = "${map("Name", "${var.stackname}-cache", "Project", var.stackname, "aws_environment", var.aws_environment, "aws_migration", "cache")}"
 }
 
 resource "aws_route53_record" "cache_service_record" {
@@ -80,21 +87,21 @@ resource "aws_route53_record" "cache_service_record" {
 }
 
 module "cache" {
-  source                               = "../../modules/aws/node_group"
-  name                                 = "${var.stackname}-cache"
-  vpc_id                               = "${data.terraform_remote_state.infra_vpc.vpc_id}"
-  default_tags                         = "${map("Project", var.stackname, "aws_stackname", var.stackname, "aws_migration", "cache", "aws_hostname", "cache-1")}"
-  instance_subnet_ids                  = "${data.terraform_remote_state.infra_networking.private_subnet_ids}"
-  instance_security_group_ids          = ["${data.terraform_remote_state.infra_security_groups.sg_cache_id}", "${data.terraform_remote_state.infra_security_groups.sg_management_id}"]
-  instance_type                        = "t2.medium"
-  create_instance_key                  = true
-  instance_key_name                    = "${var.stackname}-cache"
-  instance_public_key                  = "${var.ssh_public_key}"
-  instance_additional_user_data_script = "${file("${path.module}/additional_user_data.txt")}"
-  instance_elb_ids                     = ["${aws_elb.cache_elb.id}"]
-  asg_max_size                         = "3"
-  asg_min_size                         = "3"
-  asg_desired_capacity                 = "3"
+  source                        = "../../modules/aws/node_group"
+  name                          = "${var.stackname}-cache"
+  vpc_id                        = "${data.terraform_remote_state.infra_vpc.vpc_id}"
+  default_tags                  = "${map("Project", var.stackname, "aws_stackname", var.stackname, "aws_environment", var.aws_environment, "aws_migration", "cache", "aws_hostname", "cache-1")}"
+  instance_subnet_ids           = "${data.terraform_remote_state.infra_networking.private_subnet_ids}"
+  instance_security_group_ids   = ["${data.terraform_remote_state.infra_security_groups.sg_cache_id}", "${data.terraform_remote_state.infra_security_groups.sg_management_id}"]
+  instance_type                 = "t2.medium"
+  create_instance_key           = true
+  instance_key_name             = "${var.stackname}-cache"
+  instance_public_key           = "${var.ssh_public_key}"
+  instance_additional_user_data = "${join("\n", null_resource.user_data.*.triggers.snippet)}"
+  instance_elb_ids              = ["${aws_elb.cache_elb.id}"]
+  asg_max_size                  = "3"
+  asg_min_size                  = "3"
+  asg_desired_capacity          = "3"
 }
 
 # Outputs
