d7f814eff11f9f46f7b114ad47ee50bdd72a524b
Dean Wilson <dean.wilson@digital.cabinet-office.gov.uk>
2017-07-20 16:45:54 +0100

    Add draft-content-store machines

---
+++ terraform/projects/app-draft-content-store/main.tf
@@ -0,0 +1,117 @@
+# == Manifest: projects::app-draft-content-store
+#
+# draft-content-store node
+#
+# === Variables:
+#
+# aws_region
+# stackname
+# aws_environment
+# ssh_public_key
+#
+# === Outputs:
+#
+
+variable "aws_region" {
+  type        = "string"
+  description = "AWS region"
+  default     = "eu-west-1"
+}
+
+variable "stackname" {
+  type        = "string"
+  description = "Stackname"
+}
+
+variable "aws_environment" {
+  type        = "string"
+  description = "AWS Environment"
+}
+
+variable "ssh_public_key" {
+  type        = "string"
+  description = "draft-content-store default public key material"
+}
+
+# Resources
+# --------------------------------------------------------------
+terraform {
+  backend          "s3"             {}
+  required_version = "= 0.9.10"
+}
+
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+resource "aws_elb" "draft-content-store_external_elb" {
+  name            = "${var.stackname}-draft-content-store"
+  subnets         = ["${data.terraform_remote_state.infra_networking.public_subnet_ids}"]
+  security_groups = ["${data.terraform_remote_state.infra_security_groups.sg_offsite_ssh_id}"]
+  internal        = "false"
+
+  listener {
+    instance_port     = "443"
+    instance_protocol = "tcp"
+    lb_port           = "443"
+    lb_protocol       = "tcp"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "TCP:443"
+    interval            = 30
+  }
+
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = "${map("Name", "${var.stackname}-draft-content-store", "Project", var.stackname, "aws_environment", var.aws_environment, "aws_migration", "draft_content_store")}"
+}
+
+resource "aws_route53_record" "service_record" {
+  zone_id = "${data.terraform_remote_state.infra_stack_dns_zones.external_zone_id}"
+  name    = "draft-content-store.${data.terraform_remote_state.infra_stack_dns_zones.external_domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_elb.draft-content-store_external_elb.dns_name}"
+    zone_id                = "${aws_elb.draft-content-store_external_elb.zone_id}"
+    evaluate_target_health = true
+  }
+}
+
+module "draft-content-store" {
+  source                        = "../../modules/aws/node_group"
+  name                          = "${var.stackname}-draft-content-store"
+  vpc_id                        = "${data.terraform_remote_state.infra_vpc.vpc_id}"
+  default_tags                  = "${map("Project", var.stackname, "aws_stackname", var.stackname, "aws_environment", var.aws_environment, "aws_migration", "draft_content_store", "aws_hostname", "draft-content-store-1")}"
+  instance_subnet_ids           = "${data.terraform_remote_state.infra_networking.private_subnet_ids}"
+  instance_security_group_ids   = ["${data.terraform_remote_state.infra_security_groups.sg_draft-content-store_id}", "${data.terraform_remote_state.infra_security_groups.sg_management_id}"]
+  instance_type                 = "t2.micro"
+  create_instance_key           = true
+  instance_key_name             = "${var.stackname}-draft-content-store"
+  instance_public_key           = "${var.ssh_public_key}"
+  instance_additional_user_data = "${join("\n", null_resource.user_data.*.triggers.snippet)}"
+  instance_elb_ids              = ["${aws_elb.draft-content-store_external_elb.id}"]
+  asg_max_size                  = "2"
+  asg_min_size                  = "2"
+  asg_desired_capacity          = "2"
+}
+
+# Outputs
+# --------------------------------------------------------------
+
+output "draft-content-store_elb_address" {
+  value       = "${aws_elb.draft-content-store_external_elb.dns_name}"
+  description = "AWS' internal DNS name for the draft-content-store ELB"
+}
+
+output "service_dns_name" {
+  value       = "${aws_route53_record.service_record.name}"
+  description = "DNS name to access the node service"
+}
