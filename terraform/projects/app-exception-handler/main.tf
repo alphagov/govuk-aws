b2cec28b376d258e12a6b3ad45a5ebc53135ff2b
Sam Cook <sam.cook@digital.cabinet-office.gov.uk>
2017-07-21 15:52:07 +0100

    Add exception handler
    This correctly brings up an ASG with 2 attached volumes

---
+++ terraform/projects/app-exception-handler/main.tf
@@ -0,0 +1,210 @@
+# == Manifest: projects::app-exception-handler
+#
+# Exception Handler node
+#
+# === Variables:
+#
+# aws_region
+# stackname
+# aws_environment
+# ssh_public_key
+# exception_handler_1_subnet
+#
+# === Outputs:
+#
+# exception_handler_internal_service_dns_name
+# exception_handler_external_elb_dns_name
+#
+
+variable "aws_region" {
+  type        = "string"
+  description = "AWS region"
+  default     = "eu-west-1"
+}
+
+variable "stackname" {
+  type        = "string"
+  description = "Stackname"
+}
+
+variable "aws_environment" {
+  type        = "string"
+  description = "AWS Environment"
+}
+
+variable "ssh_public_key" {
+  type        = "string"
+  description = "Default public key material"
+}
+
+variable "exception_handler_1_subnet" {
+  type        = "string"
+  description = "Name of the subnet to place the exception_handler instance 1 and EBS volume"
+}
+
+# Resources
+# --------------------------------------------------------------
+terraform {
+  backend          "s3"             {}
+  required_version = "= 0.9.10"
+}
+
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+resource "aws_elb" "exception_handler_external_elb" {
+  name            = "${var.stackname}-exception-handler-external"
+  subnets         = ["${data.terraform_remote_state.infra_networking.public_subnet_ids}"]
+  security_groups = ["${data.terraform_remote_state.infra_security_groups.sg_exception_handler_external_elb_id}"]
+  internal        = "false"
+
+  listener {
+    instance_port     = 3029
+    instance_protocol = "tcp"
+    lb_port           = 443
+    lb_protocol       = "tcp"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+
+    target   = "TCP:3029"
+    interval = 30
+  }
+
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = "${map("Name", "${var.stackname}-exception_handler-external", "Project", var.stackname, "aws_environment", var.aws_environment, "aws_migration", "exception_handler")}"
+}
+
+resource "aws_route53_record" "exception_handler_external_service_record" {
+  zone_id = "${data.terraform_remote_state.infra_stack_dns_zones.external_zone_id}"
+  name    = "errbit.${data.terraform_remote_state.infra_stack_dns_zones.external_domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_elb.exception_handler_external_elb.dns_name}"
+    zone_id                = "${aws_elb.exception_handler_external_elb.zone_id}"
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_elb" "exception_handler_internal_elb" {
+  name            = "${var.stackname}-exception-handler-internal"
+  subnets         = ["${data.terraform_remote_state.infra_networking.private_subnet_ids}"]
+  security_groups = ["${data.terraform_remote_state.infra_security_groups.sg_exception_handler_internal_elb_id}"]
+  internal        = "true"
+
+  listener {
+    instance_port     = 3029
+    instance_protocol = "tcp"
+    lb_port           = 443
+    lb_protocol       = "tcp"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+
+    target   = "TCP:3029"
+    interval = 30
+  }
+
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = "${map("Name", "${var.stackname}-exception_handler-internal", "Project", var.stackname, "aws_environment", var.aws_environment, "aws_migration", "exception_handler")}"
+}
+
+resource "aws_route53_record" "exception_handler_internal_service_record" {
+  zone_id = "${data.terraform_remote_state.infra_stack_dns_zones.internal_zone_id}"
+  name    = "errbit.${data.terraform_remote_state.infra_stack_dns_zones.internal_domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_elb.exception_handler_internal_elb.dns_name}"
+    zone_id                = "${aws_elb.exception_handler_internal_elb.zone_id}"
+    evaluate_target_health = true
+  }
+}
+
+module "exception_handler" {
+  source                        = "../../modules/aws/node_group"
+  name                          = "${var.stackname}-exception_handler-1"
+  vpc_id                        = "${data.terraform_remote_state.infra_vpc.vpc_id}"
+  default_tags                  = "${map("Project", var.stackname, "aws_stackname", var.stackname, "aws_environment", var.aws_environment, "aws_migration", "exception_handler", "aws_hostname", "exception_handler-1")}"
+  instance_subnet_ids           = "${matchkeys(values(data.terraform_remote_state.infra_networking.private_subnet_names_ids_map), keys(data.terraform_remote_state.infra_networking.private_subnet_names_ids_map), list(var.exception_handler_1_subnet))}"
+  instance_security_group_ids   = ["${data.terraform_remote_state.infra_security_groups.sg_exception_handler_id}", "${data.terraform_remote_state.infra_security_groups.sg_management_id}"]
+  instance_type                 = "t2.medium"
+  create_instance_key           = true
+  instance_key_name             = "${var.stackname}-exception_handler-1"
+  instance_public_key           = "${var.ssh_public_key}"
+  instance_additional_user_data = "${join("\n", null_resource.user_data.*.triggers.snippet)}"
+  instance_elb_ids              = ["${aws_elb.exception_handler_internal_elb.id}", "${aws_elb.exception_handler_external_elb.id}"]
+  root_block_device_volume_size = "20"
+}
+
+resource "aws_ebs_volume" "exception_handler-1-mongodb-data" {
+  availability_zone = "${lookup(data.terraform_remote_state.infra_networking.private_subnet_names_azs_map, var.exception_handler_1_subnet)}"
+  size              = 32
+  type              = "gp2"
+
+  tags {
+    Name            = "${var.stackname}-exception-handler-1-data"
+    Project         = "${var.stackname}"
+    Device          = "xvdf"
+    aws_stackname   = "${var.stackname}"
+    aws_environment = "${var.aws_environment}"
+    aws_migration   = "exception_handler"
+    aws_hostname    = "exception_handler-1"
+  }
+}
+
+resource "aws_ebs_volume" "exception_handler-1-mongodb-backup" {
+  availability_zone = "${lookup(data.terraform_remote_state.infra_networking.private_subnet_names_azs_map, var.exception_handler_1_subnet)}"
+  size              = 64
+  type              = "gp2"
+
+  tags {
+    Name            = "${var.stackname}-exception-handler-1-mongodb"
+    Project         = "${var.stackname}"
+    Device          = "xvdg"
+    aws_stackname   = "${var.stackname}"
+    aws_environment = "${var.aws_environment}"
+    aws_migration   = "exception_handler"
+    aws_hostname    = "exception_handler-1"
+  }
+}
+
+resource "aws_iam_policy" "exception_handler_1_iam_policy" {
+  name   = "${var.stackname}-exception_handler-1-additional"
+  path   = "/"
+  policy = "${file("${path.module}/additional_policy.json")}"
+}
+
+resource "aws_iam_role_policy_attachment" "exception_handler_1_iam_role_policy_attachment" {
+  role       = "${module.exception_handler-1.instance_iam_role_name}"
+  policy_arn = "${aws_iam_policy.exception_handler_1_iam_policy.arn}"
+}
+
+# Outputs
+# --------------------------------------------------------------
+
+output "exception_handler_internal_service_dns_name" {
+  value       = "${aws_route53_record.exception_handler_internal_service_record.fqdn}"
+  description = "DNS name to access the exception_handler internal service"
+}
+
+output "exception_handler_external_elb_dns_name" {
+  value       = "${aws_route53_record.exception_handler_external_service_record.fqdn}"
+  description = "DNS name to access the exception_handler external service"
+}
---
+++ terraform/projects/infra-security-groups/exception-handler.tf
@@ -0,0 +1,116 @@
+#
+# == Manifest: Project: Security Groups: exception_handler
+#
+# The exception_handler needs to be accessible on ports:
+#   - 443/tcp internally & externally (to/from external ELBs)
+#
+# === Variables:
+# stackname - string
+#
+# === Outputs:
+# sg_exception_handler_id
+# sg_exception_handler_elb_id
+
+resource "aws_security_group" "exception_handler" {
+  name        = "${var.stackname}_exception_handler_access"
+  vpc_id      = "${data.terraform_remote_state.infra_vpc.vpc_id}"
+  description = "Access to the exception_handler host from its ELB"
+
+  tags {
+    Name = "${var.stackname}_exception_handler_access"
+  }
+}
+
+resource "aws_security_group_rule" "allow_exception_handler_external_elb_in" {
+  type      = "ingress"
+  from_port = 3029
+  to_port   = 3029
+  protocol  = "tcp"
+
+  # Which security group is the rule assigned to
+  security_group_id = "${aws_security_group.exception_handler.id}"
+
+  # Which security group can use this rule
+  source_security_group_id = "${aws_security_group.exception_handler_external_elb.id}"
+}
+
+resource "aws_security_group_rule" "allow_exception_handler_internal_elb_in" {
+  type      = "ingress"
+  from_port = 3029
+  to_port   = 3029
+  protocol  = "tcp"
+
+  # Which security group is the rule assigned to
+  security_group_id = "${aws_security_group.exception_handler.id}"
+
+  # Which security group can use this rule
+  source_security_group_id = "${aws_security_group.exception_handler_internal_elb.id}"
+}
+
+resource "aws_security_group" "exception_handler_external_elb" {
+  name        = "${var.stackname}_exception_handler_external_elb_access"
+  vpc_id      = "${data.terraform_remote_state.infra_vpc.vpc_id}"
+  description = "Access the exception_handler External ELB"
+
+  tags {
+    Name = "${var.stackname}_exception_handler_external_elb_access"
+  }
+}
+
+resource "aws_security_group_rule" "allow_exception_handler_external_elb_office_in" {
+  type      = "ingress"
+  from_port = 443
+  to_port   = 443
+  protocol  = "tcp"
+
+  security_group_id = "${aws_security_group.exception_handler_external_elb.id}"
+  cidr_blocks       = ["${var.office_ips}"]
+}
+
+resource "aws_security_group_rule" "allow_exception_handler_external_elb_management_in" {
+  type      = "ingress"
+  from_port = 443
+  to_port   = 443
+  protocol  = "tcp"
+
+  security_group_id        = "${aws_security_group.exception_handler_external_elb.id}"
+  source_security_group_id = "${aws_security_group.management.id}"
+}
+
+resource "aws_security_group_rule" "allow_exception_handler_external_elb_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.exception_handler_external_elb.id}"
+}
+
+resource "aws_security_group" "exception_handler_internal_elb" {
+  name        = "${var.stackname}_exception_handler_internal_elb_access"
+  vpc_id      = "${data.terraform_remote_state.infra_vpc.vpc_id}"
+  description = "Access the exception_handler Internal ELB"
+
+  tags {
+    Name = "${var.stackname}_exception_handler_internal_elb_access"
+  }
+}
+
+resource "aws_security_group_rule" "allow_management_to_exception_handler_carbon_aggregator_line_elb" {
+  type      = "ingress"
+  from_port = 443
+  to_port   = 443
+  protocol  = "tcp"
+
+  security_group_id        = "${aws_security_group.exception_handler_internal_elb.id}"
+  source_security_group_id = "${aws_security_group.management.id}"
+}
+
+resource "aws_security_group_rule" "allow_exception_handler_internal_elb_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.exception_handler_internal_elb.id}"
+}
