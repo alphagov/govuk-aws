07d48140064995d9a11d5ede1fde7790ab411d44
Dean Wilson <dean.wilson@digital.cabinet-office.gov.uk>
2017-08-07 18:54:28 +0100

    Allow the backend lb machines to connect to the backend ELB

--- terraform/projects/infra-security-groups/backend.tf
+++ terraform/projects/infra-security-groups/backend.tf
@@ -44,6 +44,19 @@ resource "aws_security_group" "backend_elb" {
   }
 }
 
+resource "aws_security_group_rule" "allow_backend-lb_in_backend_elb" {
+  type      = "ingress"
+  from_port = 443
+  to_port   = 443
+  protocol  = "tcp"
+
+  # Which security group is the rule assigned to
+  security_group_id = "${aws_security_group.backend_elb.id}"
+
+  # Which security group can use this rule
+  source_security_group_id = "${aws_security_group.backend-lb.id}"
+}
+
 # TODO: replace this with ingress from the backend LBs when we build them.
 resource "aws_security_group_rule" "allow_management_to_backend_elb" {
   type      = "ingress"
