07e023942fe4b59b353f4e9bd3edabb6679a6193
Laura Martin <surminus@gmail.com>
2017-08-14 22:17:41 +0100

    Publishing API needs access to the PostgreSQL instance

--- terraform/projects/infra-security-groups/postgresql.tf
+++ terraform/projects/infra-security-groups/postgresql.tf
@@ -18,7 +18,7 @@ resource "aws_security_group" "postgresql-primary" {
   }
 }
 
-resource "aws_security_group_rule" "allow_postgresql_clients_in" {
+resource "aws_security_group_rule" "allow_postgresql_from_backend_in" {
   type      = "ingress"
   from_port = 5432
   to_port   = 5432
@@ -29,6 +29,19 @@ resource "aws_security_group_rule" "allow_postgresql_clients_in" {
 
   # Which security group can use this rule
   source_security_group_id = "${aws_security_group.backend.id}"
+}
+
+resource "aws_security_group_rule" "allow_postgresql_from_publishing-api_in" {
+  type      = "ingress"
+  from_port = 5432
+  to_port   = 5432
+  protocol  = "tcp"
+
+  # Which security group is the rule assigned to
+  security_group_id = "${aws_security_group.postgresql-primary.id}"
+
+  # Which security group can use this rule
+  source_security_group_id = "${aws_security_group.publishing-api.id}"
 }
 
 resource "aws_security_group_rule" "allow_postgresql_db-admin_in" {
