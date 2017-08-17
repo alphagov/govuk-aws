27b21a8f0d4eeee54db3cbc73fbae66575e9a2d4
Sam Cook <sam.cook@digital.cabinet-office.gov.uk>
2017-08-17 14:55:02 +0100

    Allow ssh from carrenza integration
    This allows SSH from the carrenza integration environment for the
    purpose of syncing data

--- terraform/projects/infra-security-groups/offsite_ssh.tf
+++ terraform/projects/infra-security-groups/offsite_ssh.tf
@@ -26,7 +26,7 @@ resource "aws_security_group_rule" "allow_offsite_ssh" {
   from_port   = 22
   to_port     = 22
   protocol    = "tcp"
-  cidr_blocks = ["${var.office_ips}"]
+  cidr_blocks = ["${concat(var.office_ips, var.carrenza_integration_ips)}"]
 
   security_group_id = "${aws_security_group.offsite_ssh.id}"
 }
