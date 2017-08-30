e58c598b3b5c3e92e8c030f422427060db2abb63
Dean Wilson <dean.wilson@digital.cabinet-office.gov.uk>
2017-08-30 15:33:37 +0100

    Add log a log retention setting for our flow logs.

--- terraform/projects/infra-flow-logs/main.tf
+++ terraform/projects/infra-flow-logs/main.tf
@@ -6,6 +6,8 @@
 #
 # aws_region
 # stackname
+# traffic_type
+# log_retention
 #
 # === Outputs:
 #
@@ -29,6 +31,12 @@ variable "traffic_type" {
   default     = "REJECT"
 }
 
+variable "log_retention" {
+  type        = "string"
+  description = "Number of days to retain flow logs for"
+  default     = "3"
+}
+
 # Resources
 # --------------------------------------------------------------
 terraform {
@@ -48,7 +56,8 @@ resource "aws_flow_log" "vpc_flow_log" {
 }
 
 resource "aws_cloudwatch_log_group" "vpc_flow_log" {
-  name = "vpc_flow_logs-${var.stackname}"
+  name              = "vpc_flow_logs-${var.stackname}"
+  retention_in_days = "${var.log_retention}"
 }
 
 resource "aws_iam_role" "vpc_flow_logs_role" {
@@ -71,34 +80,21 @@ resource "aws_iam_role" "vpc_flow_logs_role" {
 EOF
 }
 
-resource "aws_iam_role_policy" "vpc_flow_logs_policy" {
-  name = "vpc_flow_logs_policy-${var.stackname}"
-  role = "${aws_iam_role.vpc_flow_logs_role.id}"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents",
-        "logs:DescribeLogGroups",
-        "logs:DescribeLogStreams"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
+resource "aws_iam_policy" "vpc_flow_logs_policy" {
+  name   = "vpc_flow_logs_policy-${var.stackname}"
+  path   = "/"
+  policy = "${file("${path.module}/vpc_flow_logs_policy.json")}"
 }
-EOF
+
+resource "aws_iam_role_policy_attachment" "vpc_flow_logs_policy_attachment" {
+  role       = "${aws_iam_role.vpc_flow_logs_role.name}"
+  policy_arn = "${aws_iam_policy.vpc_flow_logs_policy.arn}"
 }
 
 # Outputs
 # --------------------------------------------------------------
 
-output "docker_management_etcd_elb_dns_name" {
+output "flow-log-id" {
   value       = "${aws_flow_log.vpc_flow_log.id}"
   description = "AWS VPC Flog log ID"
 }
