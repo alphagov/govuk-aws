8b4846cf86e7c88323c70590c43b434d8d62ec6f
Ana Fernandez <ana.fernandez@digital.cabinet-office.gov.uk>
2017-09-22 17:42:14 +0100

    Add EBS to deploy
    Add external disk to store Jenkins data.

--- terraform/projects/app-deploy/main.tf
+++ terraform/projects/app-deploy/main.tf
@@ -41,9 +41,14 @@ variable "instance_ami_filter_name" {
   default     = ""
 }
 
-variable "elb_certname" {
+variable "elb_external_certname" {
   type        = "string"
   description = "The ACM cert domain name to find the ARN of"
+}
+
+variable "deploy_subnet" {
+  type        = "string"
+  description = "Name of the subnet to place the apt instance 1 and EBS volume"
 }
 
 variable "remote_state_infra_artefact_bucket_stack" {
@@ -75,8 +80,8 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
-data "aws_acm_certificate" "elb_cert" {
-  domain   = "${var.elb_certname}"
+data "aws_acm_certificate" "elb_external_cert" {
+  domain   = "${var.elb_external_certname}"
   statuses = ["ISSUED"]
 }
 
@@ -92,7 +97,7 @@ resource "aws_elb" "deploy_elb" {
     lb_port           = 443
     lb_protocol       = "https"
 
-    ssl_certificate_id = "${data.aws_acm_certificate.elb_cert.arn}"
+    ssl_certificate_id = "${data.aws_acm_certificate.elb_external_cert.arn}"
   }
 
   health_check {
@@ -112,22 +117,6 @@ resource "aws_elb" "deploy_elb" {
   tags = "${map("Name", "${var.stackname}-deploy", "Project", var.stackname, "aws_environment", var.aws_environment, "aws_migration", "jenkins")}"
 }
 
-module "deploy" {
-  source                        = "../../modules/aws/node_group"
-  name                          = "${var.stackname}-deploy"
-  vpc_id                        = "${data.terraform_remote_state.infra_vpc.vpc_id}"
-  default_tags                  = "${map("Project", var.stackname, "aws_stackname", var.stackname, "aws_environment", var.aws_environment, "aws_migration", "jenkins", "aws_hostname", "jenkins-1")}"
-  instance_subnet_ids           = "${data.terraform_remote_state.infra_networking.private_subnet_ids}"
-  instance_security_group_ids   = ["${data.terraform_remote_state.infra_security_groups.sg_deploy_id}", "${data.terraform_remote_state.infra_security_groups.sg_management_id}"]
-  instance_type                 = "t2.medium"
-  create_instance_key           = true
-  instance_key_name             = "${var.stackname}-deploy"
-  instance_public_key           = "${var.ssh_public_key}"
-  instance_additional_user_data = "${join("\n", null_resource.user_data.*.triggers.snippet)}"
-  instance_elb_ids              = ["${aws_elb.deploy_elb.id}"]
-  instance_ami_filter_name      = "${var.instance_ami_filter_name}"
-}
-
 resource "aws_route53_record" "service_record" {
   zone_id = "${data.terraform_remote_state.infra_stack_dns_zones.external_zone_id}"
   name    = "deploy.${data.terraform_remote_state.infra_stack_dns_zones.external_domain_name}"
@@ -138,6 +127,49 @@ resource "aws_route53_record" "service_record" {
     zone_id                = "${aws_elb.deploy_elb.zone_id}"
     evaluate_target_health = true
   }
+}
+
+module "deploy" {
+  source                        = "../../modules/aws/node_group"
+  name                          = "${var.stackname}-deploy"
+  vpc_id                        = "${data.terraform_remote_state.infra_vpc.vpc_id}"
+  default_tags                  = "${map("Project", var.stackname, "aws_stackname", var.stackname, "aws_environment", var.aws_environment, "aws_migration", "jenkins", "aws_hostname", "jenkins-1")}"
+  instance_subnet_ids           = "${matchkeys(values(data.terraform_remote_state.infra_networking.private_subnet_names_ids_map), keys(data.terraform_remote_state.infra_networking.private_subnet_names_ids_map), list(var.deploy_subnet))}"
+  instance_security_group_ids   = ["${data.terraform_remote_state.infra_security_groups.sg_deploy_id}", "${data.terraform_remote_state.infra_security_groups.sg_management_id}"]
+  instance_type                 = "t2.medium"
+  create_instance_key           = true
+  instance_key_name             = "${var.stackname}-deploy"
+  instance_public_key           = "${var.ssh_public_key}"
+  instance_additional_user_data = "${join("\n", null_resource.user_data.*.triggers.snippet)}"
+  instance_elb_ids              = ["${aws_elb.deploy_elb.id}"]
+  instance_ami_filter_name      = "${var.instance_ami_filter_name}"
+}
+
+resource "aws_ebs_volume" "deploy" {
+  availability_zone = "${lookup(data.terraform_remote_state.infra_networking.private_subnet_names_azs_map, var.deploy_subnet)}"
+  size              = 40
+  type              = "gp2"
+
+  tags {
+    Name            = "${var.stackname}-deploy"
+    Project         = "${var.stackname}"
+    Device          = "xvdf"
+    aws_hostname    = "jenkins-1"
+    aws_migration   = "jenkins"
+    aws_stackname   = "${var.stackname}"
+    aws_environment = "${var.aws_environment}"
+  }
+}
+
+resource "aws_iam_policy" "deploy_iam_policy" {
+  name   = "${var.stackname}-deploy-additional"
+  path   = "/"
+  policy = "${file("${path.module}/additional_policy.json")}"
+}
+
+resource "aws_iam_role_policy_attachment" "deploy_iam_role_policy_attachment" {
+  role       = "${module.deploy.instance_iam_role_name}"
+  policy_arn = "${aws_iam_policy.deploy_iam_policy.arn}"
 }
 
 # Outputs
