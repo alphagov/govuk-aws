d375ec9948ecfacd3b58e5640d7c505eb65945b0
Laura Martin <surminus@gmail.com>
2017-09-29 15:52:58 +0100

    Create frontend swarm cluster
    This creates the "docker-frontend" project, which includes:
    
    An ASG comprised of 2 workers
    An ASG comprised of a single manager
    
    They are joined to the same security group that allows full cluster
    communications.
    
    The idea is that a working Docker Swarm cluster could entirely replace
    our frontend application instances (currently frontend,
    calculators-frontend and content-store).
    
    If a machine goes away, then it should hopefully be able to rejoin the
    cluster and continue running the services it requires in that
    environment.

---
+++ terraform/projects/app-docker-frontend/main.tf
@@ -0,0 +1,167 @@
+# == Manifest: projects::app-docker-frontend
+#
+# Docker Frontend application cluster
+#
+# === Variables:
+#
+# aws_region
+# stackname
+# aws_environment
+# ssh_public_key
+# instance_ami_filter_name
+# elb_internal_certname
+# app_service_records
+#
+# === Outputs:
+#
+
+variable "aws_region" {
+  type        = "string"
+  description = "AWS region"
+  default     = "eu-west-1"
+}
+
+variable "stackname" {
+  type        = "string"
+  description = "Stackname"
+}
+
+variable "aws_environment" {
+  type        = "string"
+  description = "AWS Environment"
+}
+
+variable "ssh_public_key" {
+  type        = "string"
+  description = "Default public key material"
+}
+
+variable "instance_ami_filter_name" {
+  type        = "string"
+  description = "Name to use to find AMI images"
+  default     = ""
+}
+
+variable "elb_internal_certname" {
+  type        = "string"
+  description = "The ACM cert domain name to find the ARN of"
+}
+
+variable "app_service_records" {
+  type        = "list"
+  description = "List of application service names that get traffic via this loadbalancer"
+  default     = []
+}
+
+# Resources
+# --------------------------------------------------------------
+terraform {
+  backend          "s3"             {}
+  required_version = "= 0.9.10"
+}
+
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+data "aws_acm_certificate" "elb_cert" {
+  domain   = "${var.elb_internal_certname}"
+  statuses = ["ISSUED"]
+}
+
+resource "aws_elb" "docker-frontend_elb" {
+  name            = "${var.stackname}-docker-frontend"
+  subnets         = ["${data.terraform_remote_state.infra_networking.private_subnet_ids}"]
+  security_groups = ["${data.terraform_remote_state.infra_security_groups.sg_docker-frontend_elb_id}"]
+  internal        = "true"
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 443
+    lb_protocol       = "https"
+
+    ssl_certificate_id = "${data.aws_acm_certificate.elb_cert.arn}"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+
+    target   = "TCP:80"
+    interval = 30
+  }
+
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = "${map("Name", "${var.stackname}-docker-frontend", "Project", var.stackname, "aws_environment", var.aws_environment, "aws_migration", "docker_frontend")}"
+}
+
+resource "aws_route53_record" "service_record" {
+  zone_id = "${data.terraform_remote_state.infra_stack_dns_zones.internal_zone_id}"
+  name    = "docker-frontend.${data.terraform_remote_state.infra_stack_dns_zones.internal_domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_elb.docker-frontend_elb.dns_name}"
+    zone_id                = "${aws_elb.docker-frontend_elb.zone_id}"
+    evaluate_target_health = true
+  }
+}
+
+module "docker-worker-frontend" {
+  source                        = "../../modules/aws/node_group"
+  name                          = "${var.stackname}-docker-frontend"
+  vpc_id                        = "${data.terraform_remote_state.infra_vpc.vpc_id}"
+  default_tags                  = "${map("Project", var.stackname, "aws_stackname", var.stackname, "aws_environment", var.aws_environment, "aws_migration", "docker_worker_frontend", "aws_hostname", "docker-worker-frontend-1")}"
+  instance_subnet_ids           = "${data.terraform_remote_state.infra_networking.private_subnet_ids}"
+  instance_security_group_ids   = ["${data.terraform_remote_state.infra_security_groups.sg_docker-frontend_id}", "${data.terraform_remote_state.infra_security_groups.sg_management_id}"]
+  instance_type                 = "t2.medium"
+  create_instance_key           = true
+  instance_key_name             = "${var.stackname}-docker-frontend"
+  instance_public_key           = "${var.ssh_public_key}"
+  instance_additional_user_data = "${join("\n", null_resource.user_data.*.triggers.snippet)}"
+  instance_elb_ids              = ["${aws_elb.docker-frontend_elb.id}"]
+  instance_ami_filter_name      = "${var.instance_ami_filter_name}"
+  asg_max_size                  = "2"
+  asg_min_size                  = "2"
+  asg_desired_capacity          = "2"
+  root_block_device_volume_size = "30"
+}
+
+module "docker-manager-frontend" {
+  source                        = "../../modules/aws/node_group"
+  name                          = "${var.stackname}-docker-frontend"
+  vpc_id                        = "${data.terraform_remote_state.infra_vpc.vpc_id}"
+  default_tags                  = "${map("Project", var.stackname, "aws_stackname", var.stackname, "aws_environment", var.aws_environment, "aws_migration", "docker_manager_frontend", "aws_hostname", "docker-manager-frontend-1")}"
+  instance_subnet_ids           = "${data.terraform_remote_state.infra_networking.private_subnet_ids}"
+  instance_security_group_ids   = ["${data.terraform_remote_state.infra_security_groups.sg_docker-frontend_id}", "${data.terraform_remote_state.infra_security_groups.sg_management_id}"]
+  instance_type                 = "t2.medium"
+  create_instance_key           = true
+  instance_key_name             = "${var.stackname}-docker-frontend"
+  instance_public_key           = "${var.ssh_public_key}"
+  instance_additional_user_data = "${join("\n", null_resource.user_data.*.triggers.snippet)}"
+  instance_elb_ids              = ["${aws_elb.docker-frontend_elb.id}"]
+  instance_ami_filter_name      = "${var.instance_ami_filter_name}"
+  asg_max_size                  = "1"
+  asg_min_size                  = "1"
+  asg_desired_capacity          = "1"
+  root_block_device_volume_size = "30"
+}
+
+# Outputs
+# --------------------------------------------------------------
+
+output "docker-frontend_elb_dns_name" {
+  value       = "${aws_elb.docker-frontend_elb.dns_name}"
+  description = "DNS name to access the docker-frontend service"
+}
+
+output "service_dns_name" {
+  value       = "${aws_route53_record.service_record.name}"
+  description = "DNS name to access the node service"
+}
