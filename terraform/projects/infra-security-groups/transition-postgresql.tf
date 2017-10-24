5dc918806a9e77ad3c94195a27f1a1ba54eb4c79
Isabell Long <isabell@issyl0.co.uk>
2017-10-24 16:54:07 +0100

    Allow access to transition-postgresql-standby from bouncer

--- terraform/projects/infra-security-groups/transition-postgresql.tf
+++ terraform/projects/infra-security-groups/transition-postgresql.tf
@@ -18,6 +18,16 @@ resource "aws_security_group" "transition-postgresql-primary" {
   }
 }
 
+resource "aws_security_group" "transition-postgresql-standby" {
+  name        = "${var.stackname}_transition-postgresql-standby_access"
+  vpc_id      = "${data.terraform_remote_state.infra_vpc.vpc_id}"
+  description = "Access to transition-postgresql-standby from its clients"
+
+  tags {
+    Name = "${var.stackname}_transition-postgresql-standby_access"
+  }
+}
+
 resource "aws_security_group_rule" "allow_transition-postgresql_from_backend_in" {
   type      = "ingress"
   from_port = 5432
@@ -42,4 +52,17 @@ resource "aws_security_group_rule" "allow_transition-postgresql_db-admin_in" {
 
   # Which security group can use this rule
   source_security_group_id = "${aws_security_group.transition-db-admin.id}"
+}
+
+resource "aws_security_group_rule" "allow_transition-postgresql-standby_from_bouncer_in" {
+  type      = "ingress"
+  from_port = 5432
+  to_port   = 5432
+  protocol  = "tcp"
+
+  # Which security group is the rule assigned to
+  security_group_id = "${aws_security_group.transition-postgresql-standby.id}"
+
+  # Which security group can use this rule
+  source_security_group_id = "${aws_security_group.bouncer.id}"
 }
