23d589cc5e6b607fe5b6125e682f9c1d0e627a5e
Ana Fernandez <ana.fernandez@digital.cabinet-office.gov.uk>
2017-11-22 18:14:30 +0000

    Add module IAM role_user
    Add new AWS module to create an IAM role for trusted users from
    an external account. We follow "Account management at GDS" guide instructions.
    
    AWS docs:
    http://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_create_for-user.html

---
+++ terraform/modules/aws/iam/role_user/main.tf
@@ -0,0 +1,87 @@
+/**
+* ## Module: aws/iam/role_user
+*
+* This module creates a role with a trust relationship with IAM users from
+* a trusted account. We need to specify the list of trusted users and
+* policies to attach to the role.
+*
+* For instance, the following parameters allow the users 'user1' and 'user2'
+* from the account ID 123456789000 to use the AWS Security Token Service
+* AssumeRole API action to gain AdministratorAccess permissions on our account:
+*
+*```
+* "role_name" = "myadmins"
+*
+* "role_policy_arns" = [
+*   "arn:aws:iam::aws:policy/AdministratorAccess"
+* ]
+*
+* "role_user_arns" = [
+*   "arn:aws:iam::123456789000:user/user1",
+*   "arn:aws:iam::123456789000:user/user2",
+* ]
+*```
+*/
+variable "role_name" {
+  type        = "string"
+  description = "The name of the Role"
+}
+
+variable "role_user_arns" {
+  type        = "list"
+  description = "List of ARNs of external users that can assume the role"
+}
+
+variable "role_policy_arns" {
+  type        = "list"
+  description = "List of ARNs of policies to attach to the role"
+  default     = []
+}
+
+# Resources
+#--------------------------------------------------------------
+
+data "aws_iam_policy_document" "assume_policy_document" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${var.role_user_arns}"]
+    }
+
+    condition {
+      test     = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+
+      values = [
+        "true",
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "user_role" {
+  name               = "${var.role_name}"
+  path               = "/"
+  description        = "Role to Delegate Permissions to an IAM User: ${var.role_name}"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_policy_document.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "user_policy_attachment" {
+  count      = "${length(var.role_policy_arns)}"
+  role       = "${aws_iam_role.user_role.name}"
+  policy_arn = "${element(var.role_policy_arns, count.index)}"
+}
+
+# Outputs
+#--------------------------------------------------------------
+
+output "role_arn" {
+  value       = "${aws_iam_role.user_role.arn}"
+  description = "The ARN specifying the role."
+}
