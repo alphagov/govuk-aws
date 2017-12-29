8a741009c1e6b954d80e7d8e958e6769862d31d1
Laura Martin <surminus@gmail.com>
2017-12-29 17:52:43 +0000

    Change instances with disks to M4
    M5 instances use different device labels than M4, and we hardcode device
    names in our Puppet. We should continue to be consistent, so move back
    to using M4s with anything that attaches a disk (and Mongo because we'll
    attach a disk eventually).
    
    http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/nvme-ebs-volumes.html

--- terraform/projects/app-graphite/main.tf
+++ terraform/projects/app-graphite/main.tf
@@ -195,7 +195,7 @@ module "graphite-1" {
   default_tags                  = "${map("Project", var.stackname, "aws_stackname", var.stackname, "aws_environment", var.aws_environment, "aws_migration", "graphite", "aws_hostname", "graphite-1")}"
   instance_subnet_ids           = "${matchkeys(values(data.terraform_remote_state.infra_networking.private_subnet_names_ids_map), keys(data.terraform_remote_state.infra_networking.private_subnet_names_ids_map), list(var.graphite_1_subnet))}"
   instance_security_group_ids   = ["${data.terraform_remote_state.infra_security_groups.sg_graphite_id}", "${data.terraform_remote_state.infra_security_groups.sg_management_id}"]
-  instance_type                 = "m5.xlarge"
+  instance_type                 = "m4.xlarge"
   instance_additional_user_data = "${join("\n", null_resource.user_data.*.triggers.snippet)}"
   instance_elb_ids_length       = "2"
   instance_elb_ids              = ["${aws_elb.graphite_internal_elb.id}", "${aws_elb.graphite_external_elb.id}"]
