ec2d8f63333c85015ddfb7e89ad7a51c615b948d
Murray Steele <muz@h-lame.com>
2018-02-21 14:55:17 +0000

    Allow backend machines access to graphite
    Testing out some changes to the release app on integration I
    found that it timed out when trying to access a grafana 
    dashboard, but on staging and production it did not.  It times
    out in the "open" phase, rather than the "read" phase of the
    http request, so we figured it was a firewall issue.  Opening
    up access to graphite machines to the backend machines
    (which is where the release app lives) should mean that the
    request won't time out any more.

--- terraform/projects/infra-security-groups/graphite.tf
+++ terraform/projects/infra-security-groups/graphite.tf
@@ -148,6 +148,19 @@ resource "aws_security_group_rule" "graphite-internal-elb_ingress_deploy_https" 
   source_security_group_id = "${aws_security_group.deploy.id}"
 }
 
+resource "aws_security_group_rule" "graphite-internal-elb_ingress_backend_https" {
+  type      = "ingress"
+  from_port = 443
+  to_port   = 443
+  protocol  = "tcp"
+
+  # Which security group is the rule assigned to
+  security_group_id = "${aws_security_group.graphite_internal_elb.id}"
+
+  # Which security group can use this rule
+  source_security_group_id = "${aws_security_group.backend.id}"
+}
+
 resource "aws_security_group_rule" "graphite-internal-elb_ingress_management_carbon" {
   type      = "ingress"
   from_port = 2003
