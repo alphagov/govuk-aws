475bdee91d7ad662098871f2dcb9ac0f13314eba
Sebastian Schmieschek <sebastian@schmie.net>
2018-05-30 18:16:49 +0100

    Adding security group to allow frontend to access redis
    - Frontend requires access to redis-backend machines
    
    - This was discovered by emergency banner publication failing
    
    @schmie

--- terraform/projects/infra-security-groups/backend-redis.tf
+++ terraform/projects/infra-security-groups/backend-redis.tf
@@ -124,3 +124,16 @@ resource "aws_security_group_rule" "backend-redis_ingress_db-admin_redis" {
   # Which security group can use this rule
   source_security_group_id = "${aws_security_group.db-admin.id}"
 }
+
+resource "aws_security_group_rule" "backend-redis_ingress_frontend_redis" {
+  type      = "ingress"
+  from_port = 6379
+  to_port   = 6379
+  protocol  = "tcp"
+
+  # Which security group is the rule assigned to
+  security_group_id = "${aws_security_group.backend-redis.id}"
+
+  # Which security group can use this rule
+  source_security_group_id = "${aws_security_group.frontend.id}"
+}
