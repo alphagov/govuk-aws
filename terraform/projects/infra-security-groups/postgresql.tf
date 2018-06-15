78b3eb582fae0dc3aafbf150a31be5908fcad654
Paul Bowsher <paul.bowsher@digital.cabinet-office.gov.uk>
2018-06-15 08:03:22 +0100

    Allow CKAN to access postgres and redis

--- terraform/projects/infra-security-groups/postgresql.tf
+++ terraform/projects/infra-security-groups/postgresql.tf
@@ -31,6 +31,19 @@ resource "aws_security_group_rule" "postgresql-primary_ingress_backend_postgres"
   source_security_group_id = "${aws_security_group.backend.id}"
 }
 
+resource "aws_security_group_rule" "postgresql-primary_ingress_ckan_postgres" {
+  type      = "ingress"
+  from_port = 5432
+  to_port   = 5432
+  protocol  = "tcp"
+
+  # Which security group is the rule assigned to
+  security_group_id = "${aws_security_group.postgresql-primary.id}"
+
+  # Which security group can use this rule
+  source_security_group_id = "${aws_security_group.ckan.id}"
+}
+
 resource "aws_security_group_rule" "postgresql-primary_ingress_email-alert-api_postgres" {
   type      = "ingress"
   from_port = 5432
