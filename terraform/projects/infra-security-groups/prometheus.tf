7a7c1b0bd9d3d1f06189e3f395fbdb37762ed040
David Pye <david.pye@digital.cabinet-office.gov.uk>
2018-07-04 16:39:46 +0100

    corrected wierd lb config to be more sensible and limit to office

--- terraform/projects/infra-security-groups/prometheus.tf
+++ terraform/projects/infra-security-groups/prometheus.tf
@@ -12,35 +12,49 @@
 # sg_prometheus_id
 # sg_prometheus_elb_id
 
-resource "aws_security_group" "sg_prometheus_elb" {
-  name        = "${var.stackname}_prometheus_elb"
+resource "aws_security_group" "prometheus" {
+  name        = "${var.stackname}_prometheus"
   vpc_id      = "${data.terraform_remote_state.infra_vpc.vpc_id}"
-  description = "Access to prometheus from its ELB"
+  description = "Access to prometheus"
 
   tags {
     Name = "${var.stackname}_prometheus_elb"
   }
 }
 
-resource "aws_security_group_rule" "prometheus-elb-ingress-https" {
+resource "aws_security_group_rule" "prometheus-ingress-prometheus_external_elb-https" {
   type      = "ingress"
   from_port = 443
   to_port   = 443
   protocol  = "tcp"
 
   # Which security group is the rule assigned to
-  security_group_id = "${aws_security_group.sg_prometheus_ingress.id}"
+  security_group_id = "${aws_security_group.prometheus.id}"
 
   # Which security group can use this rule
-  source_security_group_id = "${aws_security_group.sg_prometheus_ingress.id}"
+  source_security_group_id = "${aws_security_group.prometheus_external_elb.id}"
 }
 
-resource "aws_security_group" "sg_prometheus_ingress" {
-  name        = "${var.stackname}_prometheus_ingress"
+resource "aws_security_group" "prometheus_external_elb" {
+  name        = "${var.stackname}_prometheus_external_elb"
   vpc_id      = "${data.terraform_remote_state.infra_vpc.vpc_id}"
   description = "Access to prometheus"
 
   tags {
-    Name = "${var.stackname}_prometheus_ingress"
+    Name = "${var.stackname}_prometheus_external_elb"
   }
 }
+
+resource "aws_security_group_rule" "prometheus_external_elb-ingress-office-https" {
+  type      = "ingress"
+  from_port = 443
+  to_port   = 443
+  protocol  = "tcp"
+
+  # Which security group is the rule assigned to
+  security_group_id = "${aws_security_group.prometheus_external_elb.id}"
+
+  # Which security group can use this rule
+  cidr_blocks       = ["${var.office_ips}"]
+}
+
