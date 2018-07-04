bbfe8b673842fa3112d2d13a218cbf7c8e60cba7
Kevin Dew <kevindew@me.com>
2018-07-04 16:59:11 +0100

    Glue crawler and table for GOV.UK CDN Logs
    This sets up a table which correlates with the logging pattern applied
    to fastly which is:
    
    %h\t%u\t%{%Y-%m-%d %H:%M:%S.}t%{msec_frac}t\t%{%z}t\t%m\t%{req.url}V\t%>s\t%{time.elapsed.sec}V.%{time.elapsed.msec_frac}V\t%{time.to_first_byte}V\t%B\t%{Content-Type}o\t%{User-Agent}i\t%{Fastly-Backend-Name}o\t%{server.datacenter}V\t%{if(resp.http.X-Cache ~"HIT", "HIT", "MISS")}V\t%{tls.client.protocol}V\t%{tls.client.cipher}V
    
    The components of this is explained within the code.
    
    I had initially wanted to use JSON for this as this would make it far
    easier to add/remove columns in the future but I hit a deadly bug that
    any strange characters would break through fastly `cstr_escape` function
    and leave me with broken JSON.
    
    The crawler is configured to run every 4 hours and to maintain the
    schema that is defined here rather than any changes in the data.

--- terraform/projects/infra-fastly-logs/main.tf
+++ terraform/projects/infra-fastly-logs/main.tf
@@ -125,6 +125,187 @@ resource "aws_iam_role_policy" "fastly_logs_policy" {
 EOF
 }
 
+resource "aws_glue_crawler" "govuk_www" {
+  name          = "GOV.UK fastly logs"
+  description   = "Crawls the GOV.UK logs from fastly for allowing Athena querying"
+  database_name = "${aws_glue_catalog_database.fastly_logs.name}"
+  role          = "${aws_iam_role.glue.name}"
+  schedule      = "cron(30 */4 * * ? *)"
+
+  s3_target {
+    path = "s3://${aws_s3_bucket.fastly_logs.bucket}/govuk_www"
+  }
+
+  schema_change_policy {
+    delete_behavior = "DELETE_FROM_DATABASE"
+    update_behavior = "LOG"
+  }
+
+  configuration  = <<EOF
+{
+  "Version": 1.0,
+  "CrawlerOutput": {
+    "Partitions": {
+      "AddOrUpdateBehavior": "InheritFromTable"
+    }
+  }
+}
+EOF
+}
+
+resource "aws_glue_catalog_table" "govuk_www" {
+  name          = "govuk_www"
+  description   = "Maps the tab-seperated value log file to columns"
+  database_name = "${aws_glue_catalog_database.fastly_logs.name}"
+  table_type    = "EXTERNAL_TABLE"
+
+  storage_descriptor {
+    compressed    = true
+    location      = "s3://${aws_s3_bucket.fastly_logs.bucket}/govuk_www/"
+    input_format  = "org.apache.hadoop.mapred.TextInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+
+    ser_de_info {
+      name = "ser_de_name"
+      parameters {
+        field.delim = "\t"
+      }
+      serialization_library = "org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe"
+    }
+
+    // These columns corellate with the log format set up in Fastly which is:
+    // %h\t%u\t%{%Y-%m-%d %H:%M:%S.}t%{msec_frac}t\t%{%z}t\t%m\t%{req.url}V\t%>s\tv$%{time.elapsed.sec}V.%{time.elapsed.msec_frac}V\t%{time.to_first_byte}V\t%B\t%{Content-Type}o\t%{User-Agent}i\t%{Fastly-Backend-Name}o\t%{server.datacenter}V\t%{if(resp.http.X-Cache ~"HIT", "HIT", "MISS")}V\t%{tls.client.protocol}V\t%{tls.client.cipher}V
+    columns = [
+      // %h
+      {
+        name    = "remote_host"
+        type    = "string",
+        comment = "Host that made this request, most likely an IP address"
+      },
+      // %u
+      {
+        name    = "remote_user"
+        type    = "string",
+        comment = "Basic auth user for this request"
+      },
+      // %{%Y-%m-%d %H:%M:%S.}t%{msec_frac}t
+      {
+        name    = "request_received",
+        type    = "timestamp",
+        comment = "Time we received the request"
+      },
+      // This field is separate from the timestamp above as the Presto version
+      // on AWS Athena doesn't support timestamps - expectation is that this is
+      // always +0000 though
+      // %{%z}t
+      {
+        name    = "request_received_offset",
+        type    = "string",
+        comment = "Time offset of the request, expected to be +0000 always"
+      },
+      // %m
+      {
+        name    = "method",
+        type    = "string"
+        comment = "HTTP method for this request"
+      },
+      // %{req.url}V
+      {
+        name    = "url",
+        type    = "string",
+        comment = "URL requested with query string"
+      },
+      // %>s
+      {
+        name    = "status",
+        type    = "int",
+        comment = "HTTP status code returned"
+      },
+      // $%{time.elapsed.sec}V.%{time.elapsed.msec_frac}V
+      {
+        name    = "request_time",
+        type    = "float",
+        comment = "Time until user received full response in seconds"
+      },
+      // %{time.to_first_byte}V
+      {
+        name    = "time_to_generate_response",
+        type    = "float",
+        comment = "Time spent generating a response for varnish, in seconds"
+      },
+      // %B
+      {
+        name    = "bytes",
+        type    = "bigint",
+        comment = "Number of bytes returned"
+      },
+      // %{Content-Type}o
+      {
+        name    = "content_type",
+        type    = "string",
+        comment = "HTTP Content-Type header returned"
+      },
+      // %{User-Agent}i
+      {
+        name    = "user_agent",
+        type    = "string",
+        comment = "User agent that made the request"
+      },
+      // %{Fastly-Backend-Name}o
+      {
+        name    = "fastly_backend",
+        type    = "string",
+        comment = "Name of the backend that served this request"
+      },
+      // %{server.datacenter}V
+      {
+        name    = "fastly_data_centre",
+        type    = "string",
+        comment = "Name of the data centre that served this request"
+      },
+      // %{if(resp.http.X-Cache ~"HIT", "HIT", "MISS")}V
+      {
+        name    = "cache_hit",
+        type    = "string",
+        comment = "HIT or MISS as to whether this was served from Fastly cache"
+      },
+      // %{tls.client.protocol}V
+      {
+        name = "tls_client_protocol",
+        type = "string"
+      },
+      // %{tls.client.cipher}V
+      {
+        name = "tls_client_cipher",
+        type = "string"
+      }
+    ]
+  }
+
+  // these correspond to directory ordering of:
+  // /year=YYYY/month=MM/date=DD/file.log.gz
+  partition_keys = [
+    {
+      name = "year"
+      type = "int"
+    },
+    {
+      name = "month"
+      type = "int"
+    },
+    {
+      name = "date"
+      type = "int"
+    }
+  ]
+
+  parameters {
+    classification  = "csv"
+    compressionType = "gzip"
+    delimiter       = "\t"
+  }
+}
+
 # Outputs
 # --------------------------------------------------------------
 
