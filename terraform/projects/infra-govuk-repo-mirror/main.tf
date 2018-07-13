08e868cc82bfe12b8658cd20347ce00b98a2f066
Steve Laing <steve.laing@gmail.com>
2018-07-13 11:54:42 +0100

    Add govuk-repo-mirror IAM user
    This IAM user will mirror GOVUK Github repos on AWS Code Commit.
    They require permissions to list, create and push to repositories.

---
+++ terraform/projects/infra-govuk-repo-mirror/main.tf
@@ -0,0 +1,48 @@
+/**
+* ## Module: govuk-repo-mirror
+*
+* Configures a user to allow the govuk-repo-mirror CI task
+* to push to AWS CodeCommit
+*/
+variable "aws_region" {
+  type        = "string"
+  description = "AWS region"
+  default     = "eu-west-1"
+}
+
+variable "aws_environment" {
+  type        = "string"
+  description = "AWS Environment"
+}
+
+variable "stackname" {
+  type        = "string"
+  description = "Stackname"
+}
+
+resource "aws_iam_user" "govuk_code_commit_user" {
+  name = "govuk-${var.aws_environment}-govuk-code-commit-user"
+}
+
+resource "aws_iam_policy" "govuk_code_commit_user" {
+  name        = "govuk-${var.aws_environment}-code-commit-policy"
+  policy      = "${data.aws_iam_policy_document.committer.json}"
+  description = "Allows access to AWS Code Commit"
+}
+
+resource "aws_iam_user_policy_attachment" "committer_managed_policy" {
+  user       = "${aws_iam_user.govuk_code_commit_user.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeCommitPowerUser"
+}
+
+data "aws_iam_policy_document" "committer" {
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "iam:IAMSelfManageServiceSpecificCredentials",
+      "iam:IAMReadOnlyAccess",
+    ]
+  }
+}
