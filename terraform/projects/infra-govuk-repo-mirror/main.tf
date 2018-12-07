f35c9b46a04bb57de24e11170c3f589b7ba8d785
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2018-12-07 15:46:35 +0000

    Add Jenkins SSH key to CodeCommit IAM user
    This commit adds the Jenkins SSH public key for the relevant environment to the CodeCommit IAM user. This will allow the Jenkins Deploy_App job to deploy apps from CodeCommit repositories.

--- terraform/projects/infra-govuk-repo-mirror/main.tf
+++ terraform/projects/infra-govuk-repo-mirror/main.tf
@@ -20,6 +20,11 @@ variable "stackname" {
   description = "Stackname"
 }
 
+variable "jenkins_ssh_public_key" {
+  type        = "string"
+  description = "The SSH public key of the Jenkins instance for the relevant environment"
+}
+
 terraform {
   backend          "s3"             {}
   required_version = "= 0.11.7"
@@ -37,4 +42,10 @@ resource "aws_iam_user" "govuk_code_commit_user" {
 resource "aws_iam_user_policy_attachment" "committer_managed_policy" {
   user       = "${aws_iam_user.govuk_code_commit_user.name}"
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeCommitPowerUser"
+}
+
+resource "aws_iam_user_ssh_key" "govuk_code_commit_user_jenkins_ssh_key" {
+  username   = "${aws_iam_user.govuk_code_commit_user.name}"
+  encoding   = "SSH"
+  public_key = "${var.jenkins_ssh_public_key}"
 }
