53df3b06796514228d9591f935eb688c437ccb18
Michael Walker <mike@barrucadu.co.uk>
2019-02-12 10:38:52 +0000

    Make managed elasticsearch accessible from calculators_frontend
    This is needed for licence-finder, which talks to elasticsearch
    directly (as well as talking to search-api).

--- terraform/projects/infra-security-groups/elasticsearch5.tf
+++ terraform/projects/infra-security-groups/elasticsearch5.tf
@@ -2,6 +2,7 @@
 # == Manifest: Project: Security Groups: elasticsearch5
 #
 # The elasticsearch cluster needs to be accessible on ports:
+#   - 9200 from 'calculators_frontend' (for licence-finder)
 #   - 9200 from 'search' (for search-api)
 #
 # === Variables:
@@ -19,6 +20,19 @@ resource "aws_security_group" "elasticsearch5" {
   tags {
     Name = "${var.stackname}_elasticsearch5_access"
   }
+}
+
+resource "aws_security_group_rule" "elasticsearch5_ingress_calculators-frontend_elasticsearch-api" {
+  type      = "ingress"
+  from_port = 9200
+  to_port   = 9200
+  protocol  = "tcp"
+
+  # Which security group is the rule assigned to
+  security_group_id = "${aws_security_group.elasticsearch5.id}"
+
+  # Which security group can use this rule
+  source_security_group_id = "${aws_security_group.calculators-frontend.id}"
 }
 
 resource "aws_security_group_rule" "elasticsearch5_ingress_search_elasticsearch-api" {
