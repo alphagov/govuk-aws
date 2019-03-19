3885b649d209a9ef9884ac7fccaa7a5bf7488ce1
Roch Trinqué <roch.trinque@digital.cabinet-office.gov.uk>
2019-03-19 12:51:11 +0000

    Add Carrenza facing ALB for the static app
      Following the migration of static on AWS:
    Whitehall in carrenza need a way to talk to static on AWS
    This new ALB needs to be locked down to the carrenza CIDR

--- terraform/projects/infra-security-groups/frontend.tf
+++ terraform/projects/infra-security-groups/frontend.tf
@@ -63,3 +63,70 @@ resource "aws_security_group_rule" "frontend-elb_egress_any_any" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = "${aws_security_group.frontend_elb.id}"
 }
+
+resource "aws_security_group" "static" {
+  name        = "${var.stackname}_static_access"
+  vpc_id      = "${data.terraform_remote_state.infra_vpc.vpc_id}"
+  description = "Access to static host from its ELB"
+
+  tags {
+    Name = "${var.stackname}_static_access"
+  }
+}
+
+resource "aws_security_group_rule" "frontend_ingress_static-carrenza-alb_http" {
+  type      = "ingress"
+  from_port = 80
+  to_port   = 80
+  protocol  = "tcp"
+
+  # Which security group is the rule assigned to
+  security_group_id = "${aws_security_group.frontend.id}"
+
+  # Which security group can use this rule
+  source_security_group_id = "${aws_security_group.static_carrenza_alb.id}"
+}
+
+resource "aws_security_group_rule" "static_ingress_static-carrenza-alb_http" {
+  type      = "ingress"
+  from_port = 80
+  to_port   = 80
+  protocol  = "tcp"
+
+  # Which security group is the rule assigned to
+  security_group_id = "${aws_security_group.static.id}"
+
+  # Which security group can use this rule
+  source_security_group_id = "${aws_security_group.static_carrenza_alb.id}"
+}
+
+# Security resources for ALB set up for Carrenza access to AWS calendars
+
+resource "aws_security_group" "static_carrenza_alb" {
+  name        = "${var.stackname}_static_carrenza_alb"
+  vpc_id      = "${data.terraform_remote_state.infra_vpc.vpc_id}"
+  description = "Access static Carrenza ALB "
+
+  tags {
+    Name = "${var.stackname}_static_carrenza_alb_access"
+  }
+}
+
+resource "aws_security_group_rule" "static-carrenza-alb_ingress_443_carrenza" {
+  type      = "ingress"
+  from_port = 443
+  to_port   = 443
+  protocol  = "tcp"
+
+  cidr_blocks       = ["${var.carrenza_env_ips}"]
+  security_group_id = "${aws_security_group.static_carrenza_alb.id}"
+}
+
+resource "aws_security_group_rule" "static-carrenza-alb_egress_any_any" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.static_carrenza_alb.id}"
+}
