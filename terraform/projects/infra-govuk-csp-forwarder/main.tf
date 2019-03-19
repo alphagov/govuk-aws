ae1f6a0efb4bfcaaab7f98a727ed5d846a661c2b
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2019-03-19 13:58:54 +0000

    Add govuk-csp-forwarder lambda function
    This commit creates a new lambda function and deploys the govuk-csp-forwarder to it.

---
+++ terraform/projects/infra-govuk-csp-forwarder/main.tf
@@ -0,0 +1,60 @@
+/**
+* ## Module: govuk-csp-forwarder
+*
+* Configures a role and Lambda function to collect Content Security Policy
+* reports, filter them and forward them to Sentry.
+*/
+variable "aws_region" {
+  type        = "string"
+  description = "AWS region"
+  default     = "eu-west-1"
+}
+
+variable "aws_environment" {
+  type        = "string"
+  description = "AWS Environment"
+}
+
+variable "stackname" {
+  type        = "string"
+  description = "Stackname"
+}
+
+terraform {
+  backend          "s3"             {}
+  required_version = "= 0.11.7"
+}
+
+provider "aws" {
+  region  = "${var.aws_region}"
+  version = "1.40.0"
+}
+
+resource "aws_lambda_function" "govuk_csp_forwarder_lambda_function" {
+  s3_bucket     = "govuk-integration-artefact"
+  s3_key        = "govuk-csp-forwarder/release/csp_forwarder"
+  function_name = "govuk-csp-forwarder"
+  role          = "${aws_iam_role.govuk_csp_forwarder_lambda_role.arn}"
+  handler       = "csp_forwarder"
+  runtime       = "go1.x"
+}
+
+resource "aws_iam_role" "govuk_csp_forwarder_lambda_role" {
+  name = "govuk_csp_forwarder_lambda_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
