53c252c77a0b6c8f3dde29c517474d55408fa99d
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2019-03-21 13:57:46 +0000

    Add the missing _production suffixes for the Content Data API
    To the infra-database-backups-bucket writer policy.

--- terraform/projects/infra-database-backups-bucket/writer.tf
+++ terraform/projects/infra-database-backups-bucket/writer.tf
@@ -345,11 +345,11 @@ data "aws_iam_policy_document" "content_data_api_dbadmin_database_backups_writer
     ]
 
     resources = [
-      "arn:aws:s3:::${aws_s3_bucket.database_backups.id}/content-data-api-postgresql/*-content_data_api.gz",
+      "arn:aws:s3:::${aws_s3_bucket.database_backups.id}/content-data-api-postgresql/*-content_data_api_production.gz",
 
       # The following line can be removed once the Content Data API
       # database is renamed
-      "arn:aws:s3:::${aws_s3_bucket.database_backups.id}/content-data-api-postgresql/*-content_performance_manager.gz",
+      "arn:aws:s3:::${aws_s3_bucket.database_backups.id}/content-data-api-postgresql/*-content_performance_manager_production.gz",
     ]
   }
 }
