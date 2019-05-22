e44bfac21362c279c4e43368a1b0d568051b1f37
Camille Descartes <camille.descartes@digital.cabinet-office.gov.uk>
2019-05-22 16:00:22 +0100

    Update infra-database-backups-bucket
    Add bucket policies for training environment and for
    data sync between training and integration.

--- terraform/projects/infra-database-backups-bucket/bucket_policy.tf
+++ terraform/projects/infra-database-backups-bucket/bucket_policy.tf
@@ -22,6 +22,7 @@ data "aws_iam_policy_document" "database_backups_cross_account_access" {
         "arn:aws:iam::210287912431:root",
         "arn:aws:iam::696911096973:root",
         "arn:aws:iam::172025368201:root",
+        "arn:aws:iam::291968922021:root",
       ]
     }
 
