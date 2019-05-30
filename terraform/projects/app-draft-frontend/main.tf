34f9be71f353716b6962787327242bb83149e48e
Camille Descartes <camille.descartes@digital.cabinet-office.gov.uk>
2019-05-30 16:50:16 +0100

    Enlarge volume for app-draft-frontend instances
    Add new variable to main.tf to allow for a custom volume size.
    
    One of the instances in the autoscaling group is running out of inodes
    There doesn't seem to be anything significant we can delete, so we
    are increasing the volume size.

--- terraform/projects/app-draft-frontend/main.tf
+++ terraform/projects/app-draft-frontend/main.tf
@@ -48,6 +48,12 @@ variable "enable_alb" {
   default     = false
 }
 
+variable "root_block_device_volume_size" {
+  type        = "string"
+  description = "The size of the instance root volume in gigabytes"
+  default     = "40"
+}
+
 # Resources
 # --------------------------------------------------------------
 terraform {
@@ -168,6 +174,7 @@ module "draft-frontend" {
   asg_min_size                      = "${var.asg_size}"
   asg_desired_capacity              = "${var.asg_size}"
   asg_notification_topic_arn        = "${data.terraform_remote_state.infra_monitoring.sns_topic_autoscaling_group_events_arn}"
+  root_block_device_volume_size     = "${var.root_block_device_volume_size}"
 }
 
 module "alarms-elb-draft-frontend-internal" {
