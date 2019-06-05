5c58deaf5833bf1352ec53eb7603ff82ed7f4861
Camille Descartes <camille.descartes@digital.cabinet-office.gov.uk>
2019-06-05 11:30:38 +0100

    Update app-search for Training environment
    Add backend to build app-search in the Training environment.
    
    Add parameters to select which domain to use with the DNS records (Training
    does not use the stack domain).

--- terraform/projects/app-search/main.tf
+++ terraform/projects/app-search/main.tf
@@ -54,11 +54,26 @@ variable "app_service_records" {
   default     = []
 }
 
+variable "internal_zone_name" {
+  type        = "string"
+  description = "The name of the Route53 zone that contains internal records"
+}
+
+variable "internal_domain_name" {
+  type        = "string"
+  description = "The domain name of the internal DNS records, it could be different from the zone name"
+}
+
 # Resources
 # --------------------------------------------------------------
 terraform {
   backend          "s3"             {}
   required_version = "= 0.11.7"
+}
+
+data "aws_route53_zone" "internal" {
+  name         = "${var.internal_zone_name}"
+  private_zone = true
 }
 
 provider "aws" {
@@ -110,8 +125,8 @@ resource "aws_elb" "search_elb" {
 }
 
 resource "aws_route53_record" "service_record" {
-  zone_id = "${data.terraform_remote_state.infra_stack_dns_zones.internal_zone_id}"
-  name    = "search.${data.terraform_remote_state.infra_stack_dns_zones.internal_domain_name}"
+  zone_id = "${data.aws_route53_zone.internal.zone_id}"
+  name    = "search.${var.internal_domain_name}"
   type    = "A"
 
   alias {
@@ -123,10 +138,10 @@ resource "aws_route53_record" "service_record" {
 
 resource "aws_route53_record" "app_service_records" {
   count   = "${length(var.app_service_records)}"
-  zone_id = "${data.terraform_remote_state.infra_stack_dns_zones.internal_zone_id}"
-  name    = "${element(var.app_service_records, count.index)}.${data.terraform_remote_state.infra_stack_dns_zones.internal_domain_name}"
+  zone_id = "${data.aws_route53_zone.internal.zone_id}"
+  name    = "${element(var.app_service_records, count.index)}.${var.internal_domain_name}"
   type    = "CNAME"
-  records = ["search.${data.terraform_remote_state.infra_stack_dns_zones.internal_domain_name}"]
+  records = ["search.${var.internal_domain_name}"]
   ttl     = "300"
 }
 
