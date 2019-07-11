ef027a6f47ca3184af07c5b171e121d63e53e0d6
Chris Banks <chrisbanks@gds3815.local>
2019-07-11 14:27:40 +0100

    Add CORS rule for govuk-*-mirror buckets.
    Allows cross-origin requests from any domain, for example so that images and
    other objects referenced from within pages on gov.uk will be able to load.

--- terraform/projects/infra-mirror-bucket/main.tf
+++ terraform/projects/infra-mirror-bucket/main.tf
@@ -187,6 +187,13 @@ resource "aws_s3_bucket" "govuk-mirror" {
       }
     }
   }
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "HEAD"]
+    allowed_origins = ["*"]
+    max_age_seconds = 3000
+  }
 }
 
 resource "aws_s3_bucket" "govuk-mirror-replica" {
