5941987e9eb472a29f8271745651d623beab0e8c
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2019-07-17 12:19:14 +0100

    adds waf log shipping to splunk
    Adds a firehose for forwarding web acl logs to splunk and wires up the
    "default" ACL to start shipping logs to splunk.
    
    WAF ACL Logs -> Firehose -> Splunk
    
    long term cyber will be collecting all these kinds of logs for threat
    intelligence and will be doing it by exposing a cross-account firehose
    destination that we could ship directly to. However at time of writing
    this method is not quite ready for prime-time and so we are deploying a
    firehose that will ship directly to splunk...
    
    The cyber managed Splunk instance  will ingest the logs and make them
    available both to GOV.UK members and Cyber threat intelligence.
    
    An S3 bucket acts as a backup for the firehose stream for the case when
    the splunk endpoint is unavailable.

--- terraform/projects/infra-public-services/waf.tf
+++ terraform/projects/infra-public-services/waf.tf
@@ -45,5 +45,108 @@ resource "aws_wafregional_web_acl" "default" {
     rule_id  = "${aws_wafregional_rule.x_always_block.id}"
   }
 
+  logging_configuration {
+    log_destination = "${aws_kinesis_firehose_delivery_stream.splunk.arn}"
+
+    redacted_fields {
+      field_to_match {
+        type = "URI"
+      }
+
+      field_to_match {
+        data = "referer"
+        type = "HEADER"
+      }
+    }
+  }
+
   depends_on = ["aws_wafregional_rule.x_always_block"]
+}
+
+resource "aws_s3_bucket" "aws_waf_logs" {
+  bucket = "aws-waf-logs-splunk"
+  acl    = "private"
+  bucket = "govuk-${var.aws_environment}-aws-waf-logs"
+  region = "${var.aws_region}"
+
+  lifecycle_rule {
+    id      = "all"
+    enabled = true
+
+    expiration {
+      days = 3
+    }
+  }
+}
+
+resource "aws_iam_role" "aws_waf_firehose" {
+  name = "${var.aws_environment}-aws-waf-firehose"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "firehose.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "aws_waf_firehose" {
+  name = "${var.aws_environment}-aws-waf-firehose"
+  role = "${aws_iam_role.aws_waf_firehose.id}"
+
+  policy = <<EOF
+{
+    "Statement": [
+      {
+          "Effect": "Allow",
+          "Action": [
+              "s3:AbortMultipartUpload",
+              "s3:GetBucketLocation",
+              "s3:GetObject",
+              "s3:ListBucket",
+              "s3:ListBucketMultipartUploads",
+              "s3:PutObject"
+          ],
+          "Resource": [
+              "arn:aws:s3:::${aws_s3_bucket.aws_waf_logs.bucket}",
+              "arn:aws:s3:::${aws_s3_bucket.aws_waf_logs.bucket}/*"
+          ]
+      }
+    ]
+}
+EOF
+}
+
+resource "aws_kinesis_firehose_delivery_stream" "splunk" {
+  # NOTE: The Kinesis Firehose Delivery Stream name must begin with
+  # aws-waf-logs-. See the AWS WAF Developer Guide for more information
+  # about enabling WAF logging.
+  name = "aws-waf-logs-${var.aws_environment}"
+
+  destination = "splunk"
+
+  s3_configuration {
+    role_arn           = "${aws_iam_role.aws_waf_firehose.arn}"
+    bucket_arn         = "${aws_s3_bucket.aws_waf_logs.arn}"
+    buffer_size        = 10
+    buffer_interval    = 400
+    compression_format = "GZIP"
+  }
+
+  splunk_configuration {
+    hec_endpoint               = "${var.waf_logs_hec_endpoint}"
+    hec_token                  = "${var.waf_logs_hec_token}"
+    hec_acknowledgment_timeout = 180
+    hec_endpoint_type          = "Raw"
+    s3_backup_mode             = "AllEvents"
+  }
 }
