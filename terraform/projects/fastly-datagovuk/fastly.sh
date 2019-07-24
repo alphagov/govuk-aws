3b552b4540c2c003613e9ead78685289edd9711c
Sebastian Schmieschek <sebastian@schmie.net>
2019-07-24 14:02:31 +0100

    Correction of CIDR syntac expected by Fastly VCL
    - We are introducing Terraform to automagically update Fastly IP ranges
    whitelisted in the Fastly service for data.gov.uk (DGU).
    - This commit corrects the CIDR syntax used in the VCL whitelist as the
    prior format caused terraform to error/abort.
    - I am finalising this PR as Frederic will not be available to do this,
    as he has family commitments.
    
    @schmie

--- terraform/projects/fastly-datagovuk/fastly.sh
+++ terraform/projects/fastly-datagovuk/fastly.sh
@@ -8,10 +8,10 @@ fastly_raw_ips=( $(curl https://api.fastly.com/public-ip-list 2>/dev/null | jq -
 
 fastly_ips_snippet=""
 
-for ip in "${fastly_raw_ips[@]}"
+for cidrip in "${fastly_raw_ips[@]}"
 do
-   #fastly_ips_snippet="${fastly_ips_snippet}""\\\"${ip}\\\"""; # Fastly cache node"$'\\\n  '
-   fastly_ips_snippet="${fastly_ips_snippet}"$(printf '%-22s' "\\\"${ip}\\\";")"# Fastly cache node"$'\\\n  '
+   ipstr=$(echo "${cidrip}" | awk -F  "/" '{ print("\\\""$1"\\\"""/"$2) }')
+   fastly_ips_snippet="${fastly_ips_snippet}"$(printf "%-22s %s" "${ipstr}\;" "\# Fastly cache node\n  ")
 done
 
 cp "${DIR}/datagovuk.vcl.tmp" "${DIR}/datagovuk.vcl"
