c9f228edd2e1003122ff74a752a4113d14740da9
Camille Descartes <camille.descartes@digital.cabinet-office.gov.uk>
2019-07-31 10:19:37 +0100

    Add S3 Replication to custom-formats-mapit-storage bucket
    This change ensures that this bucket now has cross region replication.

--- terraform/projects/app-custom-formats-mapit-storage/main.tf
+++ terraform/projects/app-custom-formats-mapit-storage/main.tf
@@ -29,12 +29,29 @@ variable "bucket_name" {
   default = "govuk-custom-formats-mapit-storage"
 }
 
+variable "aws_replica_region" {
+  type        = "string"
+  description = "AWS region"
+  default     = "eu-west-2"
+}
+
 # Resources
 # --------------------------------------------------------------
 
 terraform {
   backend          "s3"             {}
   required_version = "= 0.11.14"
+}
+
+provider "aws" {
+  region  = "${var.aws_region}"
+  version = "2.16.0"
+}
+
+provider "aws" {
+  region  = "${var.aws_replica_region}"
+  alias   = "aws_replica"
+  version = "2.16.0"
 }
 
 resource "aws_s3_bucket" "custom_formats_mapit" {
@@ -44,6 +61,70 @@ resource "aws_s3_bucket" "custom_formats_mapit" {
     Environment = "${var.aws_environment}"
     Team        = "custom_formats"
   }
+
+  versioning {
+    enabled = true
+  }
+
+  replication_configuration {
+    role = "${aws_iam_role.govuk_custom_formats_mapit_storage_replication_role.arn}"
+
+    rules {
+      id     = "govuk-custom-formats-mapit-storage-replication-whole-bucket-rule"
+      prefix = ""
+      status = "Enabled"
+
+      destination {
+        bucket        = "${aws_s3_bucket.custom_formats_mapit_replica.arn}"
+        storage_class = "STANDARD"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket" "custom_formats_mapit_replica" {
+  bucket   = "custom_formats_mapit_replica"
+  region   = "${var.aws_replica_region}"
+  provider = "aws.aws_replica"
+
+  tags {
+    Name            = "custom_formats_mapit_replica"
+    aws_environment = "${var.aws_environment}"
+  }
+
+  versioning {
+    enabled = true
+  }
+}
+
+data "template_file" "s3_govuk_custom_formats_mapit_storage_replication_role_template" {
+  template = "${file("${path.module}/../../policies/s3_backup_replica_role.tpl")}"
+}
+
+resource "aws_iam_role" "govuk_custom_formats_mapit_storage_replication_role" {
+  name               = "${var.stackname}-custom-formats-mapit-storage-replication-role"
+  assume_role_policy = "${data.template_file.s3_govuk_custom_formats_mapit_storage_replication_role_template.rendered}"
+}
+
+data "template_file" "s3_govuk_custom_formats_mapit_storage_replication_policy_template" {
+  template = "${file("${path.module}/../../policies/s3_backup_replica_policy.json")}"
+
+  vars {
+    govuk_s3_bucket = "${aws_s3_bucket.custom_formats_mapit.arn}"
+    govuk_s3_backup = "${aws_s3_bucket.custom_formats_mapit_replica.arn}"
+  }
+}
+
+resource "aws_iam_policy" "govuk_custom_formats_mapit_storage_replication_policy" {
+  name        = "govuk-custom-formats_mapit-storage-replication-policy"
+  policy      = "${data.template_file.s3_govuk_custom_formats_mapit_storage_replication_policy_template.rendered}"
+  description = "Allows replication of the govuk-custom-formats-mapit-storage bucket"
+}
+
+resource "aws_iam_policy_attachment" "govuk_custom_formats_mapit_storage_replication_policy_attachment" {
+  name       = "s3-govuk-custom-formats-mapit-storage-replication-policy-attachment"
+  roles      = ["${aws_iam_role.govuk_custom_formats_mapit_storage_replication_role.name}"]
+  policy_arn = "${aws_iam_policy.govuk_custom_formats_mapit_storage_replication_policy.arn}"
 }
 
 resource "aws_iam_policy" "developer_readwrite" {
