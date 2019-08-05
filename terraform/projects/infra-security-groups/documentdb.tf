557c787a82df1801e8b4f86a36a20c2f97dd7469
Camille Descartes <camille.descartes@digital.cabinet-office.gov.uk>
2019-08-05 16:59:01 +0100

    Change to security group rule
    Allow connection through db-admin

--- terraform/projects/infra-security-groups/documentdb.tf
+++ terraform/projects/infra-security-groups/documentdb.tf
@@ -18,7 +18,7 @@ resource "aws_security_group" "documentdb" {
   }
 }
 
-resource "aws_security_group_rule" "documentdb_ingress_jumpbox_mongodb" {
+resource "aws_security_group_rule" "documentdb_ingress_db-admin_mongodb" {
   type      = "ingress"
   from_port = 27017
   to_port   = 27017
@@ -28,8 +28,7 @@ resource "aws_security_group_rule" "documentdb_ingress_jumpbox_mongodb" {
   security_group_id = "${aws_security_group.documentdb.id}"
 
   # Which security group can use this rule
-  source_security_group_id = "${aws_security_group.jumpbox.id}"
+  source_security_group_id = "${aws_security_group.db-admin.id}"
 }
 
 # TODO: Add an entry for the security group containing the Licensify instances.
-
