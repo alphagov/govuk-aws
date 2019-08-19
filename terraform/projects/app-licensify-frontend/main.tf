3574a1159c29d5e1d688dceb90914370ca3ac2c0
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2019-08-19 11:49:48 +0100

    Set load balancer healthcheck paths for Licensify.
    These are the same paths as currently used by the Icinga healthchecks.
    
    `licensify-frontend` serves both the frontend UI and the `uploadlicense`
    service. There is therefore one target group and one healthcheck for
    `licensify-frontend`. In other words, we are treating the app instance
    as the failure domain for the purpose of LB healthchecking.
    
    `licensify-feed` is not load-balanced (because it's a single-master,
    manual-failover architecture).

--- terraform/projects/app-licensify-frontend/main.tf
+++ terraform/projects/app-licensify-frontend/main.tf
@@ -103,6 +103,7 @@ module "internal_lb" {
   access_logs_bucket_name          = "${data.terraform_remote_state.infra_monitoring.aws_logging_bucket_id}"
   access_logs_bucket_prefix        = "elb/licensify-frontend-internal-lb"
   listener_certificate_domain_name = "${var.elb_internal_certname}"
+  target_group_health_check_path   = "/api/licences"
 
   listener_action = {
     "HTTPS:443" = "HTTP:80"
