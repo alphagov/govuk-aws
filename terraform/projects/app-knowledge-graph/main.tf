a460bb367c515518555cdb52c43bf19dbc2ce98b
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2019-09-27 12:00:19 +0100

    Add missing SSM parameters to Knowledge Graph policy
    This commit adds missing SSM parameters to the `knowledge-graph_read_ssm_policy_document` policy for querying BigQuery to create the functional network and querying Publishing API to build the people / roles dataset.

--- terraform/projects/app-knowledge-graph/main.tf
+++ terraform/projects/app-knowledge-graph/main.tf
@@ -87,6 +87,11 @@ data "aws_iam_policy_document" "knowledge-graph_read_ssm_policy_document" {
     resources = [
       "arn:aws:ssm:eu-west-1:${data.aws_caller_identity.current.account_id}:parameter/govuk_knowledge_graph_github_deploy_key",
       "arn:aws:ssm:eu-west-1:${data.aws_caller_identity.current.account_id}:parameter/govuk_knowledge_graph_shared_password",
+      "arn:aws:ssm:eu-west-1:${data.aws_caller_identity.current.account_id}:parameter/govuk_big_query_data_service_user_key_file",
+      "arn:aws:ssm:eu-west-1:${data.aws_caller_identity.current.account_id}:parameter/govuk_knowledge_graph_publishing_api_host",
+      "arn:aws:ssm:eu-west-1:${data.aws_caller_identity.current.account_id}:parameter/govuk_knowledge_graph_publishing_api_database",
+      "arn:aws:ssm:eu-west-1:${data.aws_caller_identity.current.account_id}:parameter/govuk_knowledge_graph_publishing_api_user",
+      "arn:aws:ssm:eu-west-1:${data.aws_caller_identity.current.account_id}:parameter/govuk_knowledge_graph_publishing_api_password",
     ]
   }
 }
