04674f00c6cd1acdcabab9acd005e8190ec0b7ba
Frederic Francois <frederic.francois@digital.cabinet-office.gov.uk>
2019-11-13 16:18:30 +0000

    give dbadmin machines complete read access to s3 backups

--- terraform/projects/infra-database-backups-bucket/reader.tf
+++ terraform/projects/infra-database-backups-bucket/reader.tf
@@ -133,11 +133,9 @@ data "aws_iam_policy_document" "training_dbadmin_database_backups_reader" {
     # Need access to the top level of the tree.
     resources = [
       "arn:aws:s3:::govuk-training-database-backups",
-      "arn:aws:s3:::govuk-training-database-backups/*mysql*",
-      "arn:aws:s3:::govuk-training-database-backups/*postgres*",
+      "arn:aws:s3:::govuk-training-database-backups/*",
       "arn:aws:s3:::govuk-integration-database-backups",
-      "arn:aws:s3:::govuk-integration-database-backups/*mysql*",
-      "arn:aws:s3:::govuk-integration-database-backups/*postgres*",
+      "arn:aws:s3:::govuk-integration-database-backups/*",
     ]
   }
 }
@@ -362,13 +360,9 @@ data "aws_iam_policy_document" "integration_dbadmin_database_backups_reader" {
     # Need access to the top level of the tree.
     resources = [
       "arn:aws:s3:::govuk-integration-database-backups",
-      "arn:aws:s3:::govuk-integration-database-backups/*mongo-licensing*",
-      "arn:aws:s3:::govuk-integration-database-backups/*mysql*",
-      "arn:aws:s3:::govuk-integration-database-backups/*postgres*",
+      "arn:aws:s3:::govuk-integration-database-backups/*",
       "arn:aws:s3:::govuk-staging-database-backups",
-      "arn:aws:s3:::govuk-staging-database-backups/*mongo-licensing*",
-      "arn:aws:s3:::govuk-staging-database-backups/*mysql*",
-      "arn:aws:s3:::govuk-staging-database-backups/*postgres*",
+      "arn:aws:s3:::govuk-staging-database-backups/*",
     ]
   }
 }
@@ -580,9 +574,7 @@ data "aws_iam_policy_document" "staging_dbadmin_database_backups_reader" {
     # Need access to the top level of the tree.
     resources = [
       "arn:aws:s3:::govuk-staging-database-backups",
-      "arn:aws:s3:::govuk-staging-database-backups/*mysql*",
-      "arn:aws:s3:::govuk-staging-database-backups/*postgres*",
-      "arn:aws:s3:::govuk-staging-database-backups/*mongo-licensing*",
+      "arn:aws:s3:::govuk-staging-database-backups/*",
     ]
   }
 }
@@ -790,9 +782,7 @@ data "aws_iam_policy_document" "production_dbadmin_database_backups_reader" {
     # Need access to the top level of the tree.
     resources = [
       "arn:aws:s3:::govuk-production-database-backups",
-      "arn:aws:s3:::govuk-production-database-backups/*mysql*",
-      "arn:aws:s3:::govuk-production-database-backups/*postgres*",
-      "arn:aws:s3:::govuk-production-database-backups/*mongo-licensing*",
+      "arn:aws:s3:::govuk-production-database-backups/*",
     ]
   }
 }
