9bdbd0460f46977824e176876dd0acac467d6e0e
Erin Raj-Staniland <erin.raj-staniland@digital.cabinet-office.gov.uk>
2019-11-29 14:19:53 +0000

    Allow ITHC access to CKAN and Email Alert
    Grants SSH access to ckan and email-alert-api boxes when ithc_access_ips
    is defined

--- terraform/projects/infra-security-groups/ckan.tf
+++ terraform/projects/infra-security-groups/ckan.tf
@@ -119,3 +119,24 @@ resource "aws_security_group_rule" "ckan_ingress_db-admin_ssh" {
   security_group_id        = "${aws_security_group.ckan.id}"
   source_security_group_id = "${aws_security_group.db-admin.id}"
 }
+
+resource "aws_security_group" "ckan_ithc_access" {
+  count       = "${length(var.ithc_access_ips) > 0 ? 1 : 0}"
+  name        = "${var.stackname}_ckan_ithc_access"
+  vpc_id      = "${data.terraform_remote_state.infra_vpc.vpc_id}"
+  description = "Control access to ITHC SSH"
+
+  tags {
+    Name = "${var.stackname}_ckan_ithc_access"
+  }
+}
+
+resource "aws_security_group_rule" "ithc_ingress_ckan_ssh" {
+  count             = "${length(var.ithc_access_ips) > 0 ? 1 : 0}"
+  type              = "ingress"
+  to_port           = 22
+  from_port         = 22
+  protocol          = "tcp"
+  cidr_blocks       = "${var.ithc_access_ips}"
+  security_group_id = "${aws_security_group.ckan_ithc_access.id}"
+}
