29d715b4bfd6cb1d38d20fbff0b29b2896bb1d1a
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2020-03-17 17:49:50 +0000

    Enable Enhanced Monitoring on shared RDS Postgres.
    This is already enabled in Production; we're just reflecting the change
    in Terraform and enabling it in the other environments.

--- terraform/projects/app-postgresql/main.tf
+++ terraform/projects/app-postgresql/main.tf
@@ -131,6 +131,8 @@ module "postgresql-primary_rds_instance" {
   skip_final_snapshot   = "${var.skip_final_snapshot}"
   snapshot_identifier   = "${var.snapshot_identifier}"
   parameter_group_name  = "${aws_db_parameter_group.postgresql_pg.name}"
+  monitoring_interval   = "60"
+  monitoring_role_arn   = "${data.terraform_remote_state.infra_monitoring.rds_enhanced_monitoring_role_arn}"
 }
 
 resource "aws_route53_record" "service_record" {
