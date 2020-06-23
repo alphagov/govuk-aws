dfe75b84befaff007c841b1d339e3c647d9a303f
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2020-06-23 13:59:07 +0100

    Revert "Switch to c5.xlarge instances for the Email Alert API"
    8GB of memory should be fine for the Email Alert API, but for the last
    4 Fridays, some of the Sidekiq processes for the Email Alert API have
    used lots of memory, beyond the limit set for the Icinga checks.
    
    Until we've managed to address the excessive memory usage, and
    reliability of the jobs (Sidekiq isn't exactly reliable), going back
    to 16GB of memory might help to avoid issues with Sidekiq using all
    the memory.
    
    This reverts commit dae54679cf50585abddb6c4413b7b2a26b0492e5.

--- terraform/projects/app-email-alert-api/main.tf
+++ terraform/projects/app-email-alert-api/main.tf
@@ -49,7 +49,7 @@ variable "internal_domain_name" {
 variable "instance_type" {
   type        = "string"
   description = "Instance type used for EC2 resources"
-  default     = "c5.xlarge"
+  default     = "m5.xlarge"
 }
 
 # Resources
