5d04a57b5e0e0201ef2e306f62a03c4744b2d92a
Frederic Francois <frederic.francois@digital.cabinet-office.gov.uk>
2020-08-18 13:15:29 +0100

    initial commit for ci agents

---
+++ terraform/projects/app-ci-agents/README.md
@@ -0,0 +1,51 @@
+## Project: app-ci-agents
+
+CI agents
+
+## Requirements
+
+| Name | Version |
+|------|---------|
+| terraform | = 0.11.14 |
+| aws | 2.46.0 |
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| aws | 2.46.0 |
+| null | n/a |
+| terraform | n/a |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| aws\_environment | AWS Environment | `string` | n/a | yes |
+| aws\_region | AWS region | `string` | `"eu-west-1"` | no |
+| ci\_agent\_1\_subnet | subnet to deploy EC2 and EBS of CI agent 1 | `string` | `"govuk_private_a"` | no |
+| ebs\_encrypted | whether or not the EBS volume is encrypted | `string` | `"true"` | no |
+| elb\_internal\_certname | The ACM cert domain name to find the ARN of | `string` | n/a | yes |
+| instance\_ami\_filter\_name | Name to use to find AMI images | `string` | `""` | no |
+| instance\_type | Instance type used for EC2 resources | `string` | `"t3a.2xlarge"` | no |
+| internal\_app\_service\_records | List of application service names that get traffic via this loadbalancer | `list` | `[]` | no |
+| internal\_domain\_name | The domain name of the internal DNS records, it could be different from the zone name | `string` | n/a | yes |
+| internal\_zone\_name | The name of the Route53 zone that contains internal records | `string` | n/a | yes |
+| remote\_state\_bucket | S3 bucket we store our terraform state in | `string` | n/a | yes |
+| remote\_state\_infra\_monitoring\_key\_stack | Override stackname path to infra\_monitoring remote state | `string` | `""` | no |
+| remote\_state\_infra\_networking\_key\_stack | Override infra\_networking remote state path | `string` | `""` | no |
+| remote\_state\_infra\_root\_dns\_zones\_key\_stack | Override stackname path to infra\_root\_dns\_zones remote state | `string` | `""` | no |
+| remote\_state\_infra\_security\_groups\_key\_stack | Override infra\_security\_groups stackname path to infra\_vpc remote state | `string` | `""` | no |
+| remote\_state\_infra\_stack\_dns\_zones\_key\_stack | Override stackname path to infra\_stack\_dns\_zones remote state | `string` | `""` | no |
+| remote\_state\_infra\_vpc\_key\_stack | Override infra\_vpc remote state path | `string` | `""` | no |
+| root\_block\_device\_volume\_size | size of the root volume in GB | `string` | `"50"` | no |
+| stackname | Stackname | `string` | n/a | yes |
+| user\_data\_snippets | List of user-data snippets | `list` | n/a | yes |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| ci-agent-1\_elb\_dns\_name | DNS name to access the CI agent 1 service |
+| ci-agent-1\_service\_dns\_name | DNS name to access the CI agent 1 service |
+
---
+++ terraform/projects/app-ci-agents/main.tf
@@ -0,0 +1,234 @@
+/**
+* ## Project: app-ci-agents
+*
+* CI agents
+*/
+variable "aws_region" {
+  type        = "string"
+  description = "AWS region"
+  default     = "eu-west-1"
+}
+
+variable "stackname" {
+  type        = "string"
+  description = "Stackname"
+}
+
+variable "aws_environment" {
+  type        = "string"
+  description = "AWS Environment"
+}
+
+variable "instance_ami_filter_name" {
+  type        = "string"
+  description = "Name to use to find AMI images"
+  default     = ""
+}
+
+variable "elb_internal_certname" {
+  type        = "string"
+  description = "The ACM cert domain name to find the ARN of"
+}
+
+variable "internal_app_service_records" {
+  type        = "list"
+  description = "List of application service names that get traffic via this loadbalancer"
+  default     = []
+}
+
+variable "instance_type" {
+  type        = "string"
+  description = "Instance type used for EC2 resources"
+  default     = "t3a.2xlarge"
+}
+
+variable "internal_zone_name" {
+  type        = "string"
+  description = "The name of the Route53 zone that contains internal records"
+}
+
+variable "internal_domain_name" {
+  type        = "string"
+  description = "The domain name of the internal DNS records, it could be different from the zone name"
+}
+
+variable "root_block_device_volume_size" {
+  type        = "string"
+  description = "size of the root volume in GB"
+  default     = "50"
+}
+
+variable "ebs_encrypted" {
+  type        = "string"
+  description = "whether or not the EBS volume is encrypted"
+  default     = "true"
+}
+
+variable "ci_agent_1_subnet" {
+  type        = "string"
+  description = "subnet to deploy EC2 and EBS of CI agent 1"
+  default     = "govuk_private_a"
+}
+
+# Resources
+# --------------------------------------------------------------
+terraform {
+  backend          "s3"             {}
+  required_version = "= 0.11.14"
+}
+
+data "aws_route53_zone" "internal" {
+  name         = "${var.internal_zone_name}"
+  private_zone = true
+}
+
+provider "aws" {
+  region  = "${var.aws_region}"
+  version = "2.46.0"
+}
+
+data "aws_acm_certificate" "elb_cert" {
+  domain   = "${var.elb_internal_certname}"
+  statuses = ["ISSUED"]
+}
+
+/////////////////////ci-agent-1/////////////////////////////////////////////////
+
+resource "aws_elb" "ci-agent-1_elb" {
+  name            = "${var.stackname}-ci-agent-1"
+  subnets         = ["${data.terraform_remote_state.infra_networking.private_subnet_ids}"]
+  security_groups = ["${data.terraform_remote_state.infra_security_groups.sg_ci-agent-1_elb_id}"]
+  internal        = "true"
+
+  access_logs {
+    bucket        = "${data.terraform_remote_state.infra_monitoring.aws_logging_bucket_id}"
+    bucket_prefix = "elb/${var.stackname}-ci-agent-1-internal-elb"
+    interval      = 60
+  }
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 443
+    lb_protocol       = "https"
+
+    ssl_certificate_id = "${data.aws_acm_certificate.elb_cert.arn}"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+
+    target   = "TCP:80"
+    interval = 30
+  }
+
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = "${map("Name", "${var.stackname}-ci-agent-1", "Project", var.stackname, "aws_environment", var.aws_environment, "aws_migration", "ci-agent")}"
+}
+
+resource "aws_route53_record" "ci-agent-1_service_record" {
+  zone_id = "${data.aws_route53_zone.internal.zone_id}"
+  name    = "ci-agent-1.${var.internal_domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_elb.ci-agent-1_elb.dns_name}"
+    zone_id                = "${aws_elb.ci-agent-1_elb.zone_id}"
+    evaluate_target_health = true
+  }
+}
+
+module "ci-agent-1" {
+  source                        = "../../modules/aws/node_group"
+  name                          = "${var.stackname}-ci-agent-1"
+  default_tags                  = "${map("Project", var.stackname, "aws_stackname", var.stackname, "aws_environment", var.aws_environment, "aws_migration", "ci_agent", "aws_hostname", "ci-agent-1")}"
+  instance_subnet_ids           = "${matchkeys(values(data.terraform_remote_state.infra_networking.private_subnet_names_ids_map), keys(data.terraform_remote_state.infra_networking.private_subnet_names_ids_map), list(var.ci_agent_1_subnet))}"
+  instance_security_group_ids   = ["${data.terraform_remote_state.infra_security_groups.sg_ci-agent-1_elb_id}", "${data.terraform_remote_state.infra_security_groups.sg_management_id}"]
+  instance_type                 = "${var.instance_type}"
+  instance_additional_user_data = "${join("\n", null_resource.user_data.*.triggers.snippet)}"
+  instance_elb_ids_length       = "1"
+  instance_elb_ids              = ["${aws_elb.ci-agent-1_elb.id}"]
+  instance_ami_filter_name      = "${var.instance_ami_filter_name}"
+  asg_max_size                  = "1"
+  asg_min_size                  = "1"
+  asg_desired_capacity          = "1"
+  asg_notification_topic_arn    = "${data.terraform_remote_state.infra_monitoring.sns_topic_autoscaling_group_events_arn}"
+  root_block_device_volume_size = "${var.root_block_device_volume_size}"
+}
+
+resource "aws_ebs_volume" "ci-agent-1-data" {
+  availability_zone = "${lookup(data.terraform_remote_state.infra_networking.private_subnet_names_azs_map, var.ci_agent_1_subnet)}"
+  encrypted         = "${var.ebs_encrypted}"
+  size              = "13"
+  type              = "gp2"
+
+  tags {
+    Name            = "${var.stackname}-ci-agent-1-data"
+    Project         = "${var.stackname}"
+    Device          = "xvdf"
+    aws_hostname    = "ci-agent-1"
+    aws_migration   = "ci_agent"
+    aws_stackname   = "${var.stackname}"
+    aws_environment = "${var.aws_environment}"
+  }
+}
+
+resource "aws_ebs_volume" "ci-agent-1-docker" {
+  availability_zone = "${lookup(data.terraform_remote_state.infra_networking.private_subnet_names_azs_map, var.ci_agent_1_subnet)}"
+  encrypted         = "${var.ebs_encrypted}"
+  size              = "13"
+  type              = "gp2"
+
+  tags {
+    Name            = "${var.stackname}-ci-agent-1-docker"
+    Project         = "${var.stackname}"
+    Device          = "xvdg"
+    aws_hostname    = "ci-agent-1"
+    aws_migration   = "ci_agent"
+    aws_stackname   = "${var.stackname}"
+    aws_environment = "${var.aws_environment}"
+  }
+}
+
+resource "aws_iam_policy" "ci-agent-1_iam_policy" {
+  name   = "${var.stackname}-ci-agent-1-volume"
+  path   = "/"
+  policy = "${file("${path.module}/volume_policy.json")}"
+}
+
+resource "aws_iam_role_policy_attachment" "ci-agent-1_iam_role_policy_attachment" {
+  role       = "${module.ci-agent-1.instance_iam_role_name}"
+  policy_arn = "${aws_iam_policy.ci-agent-1_iam_policy.arn}"
+}
+
+module "alarms-elb-ci-agent-1-internal" {
+  source                         = "../../modules/aws/alarms/elb"
+  name_prefix                    = "${var.stackname}-ci-agent-1-internal"
+  alarm_actions                  = ["${data.terraform_remote_state.infra_monitoring.sns_topic_cloudwatch_alarms_arn}"]
+  elb_name                       = "${aws_elb.ci-agent-1_elb.name}"
+  httpcode_backend_4xx_threshold = "0"
+  httpcode_backend_5xx_threshold = "50"
+  httpcode_elb_4xx_threshold     = "0"
+  httpcode_elb_5xx_threshold     = "50"
+  surgequeuelength_threshold     = "0"
+  healthyhostcount_threshold     = "0"
+}
+
+# Outputs
+# --------------------------------------------------------------
+
+output "ci-agent-1_elb_dns_name" {
+  value       = "${aws_elb.ci-agent-1_elb.dns_name}"
+  description = "DNS name to access the CI agent 1 service"
+}
+
+output "ci-agent-1_service_dns_name" {
+  value       = "${aws_route53_record.ci-agent-1_service_record.name}"
+  description = "DNS name to access the CI agent 1 service"
+}
