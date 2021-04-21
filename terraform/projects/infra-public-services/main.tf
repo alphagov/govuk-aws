8ced0dc8e7614bc5ab6f845f311e633ca1b7d55a
Michael Walker <mike@barrucadu.co.uk>
2021-04-21 14:35:48 +0100

    Use readiness healthcheck for search-api public ALB

--- terraform/projects/infra-public-services/main.tf
+++ terraform/projects/infra-public-services/main.tf
@@ -2086,7 +2086,7 @@ module "search_api_public_lb" {
     "HTTPS:443" = "HTTP:80"
   }
 
-  target_group_health_check_path = "/_healthcheck"
+  target_group_health_check_path = "/_healthcheck-ready_search-api"
   subnets                        = ["${data.terraform_remote_state.infra_networking.public_subnet_ids}"]
   security_groups                = ["${data.terraform_remote_state.infra_security_groups.sg_search-api_external_elb_id}"]
   alarm_actions                  = ["${data.terraform_remote_state.infra_monitoring.sns_topic_cloudwatch_alarms_arn}"]
