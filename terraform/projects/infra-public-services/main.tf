d894e642513a7e04af00334555367a65a21e45fa
Michael Walker <mike@barrucadu.co.uk>
2021-04-21 14:34:08 +0100

    Use readiness healthcheck for bouncer public ALB

--- terraform/projects/infra-public-services/main.tf
+++ terraform/projects/infra-public-services/main.tf
@@ -713,7 +713,7 @@ module "bouncer_public_lb" {
     "HTTPS:443" = "HTTP:80"
   }
 
-  target_group_health_check_path = "/healthcheck"
+  target_group_health_check_path = "/healthcheck/ready"
   subnets                        = ["${data.terraform_remote_state.infra_networking.public_subnet_ids}"]
   security_groups                = ["${data.terraform_remote_state.infra_security_groups.sg_bouncer_elb_id}"]
   alarm_actions                  = ["${data.terraform_remote_state.infra_monitoring.sns_topic_cloudwatch_alarms_arn}"]
