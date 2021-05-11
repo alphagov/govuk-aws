cb3854f76bc0b1330d593660d140e46705908747
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2021-05-11 11:15:11 +0100

    Fix ArtefactSync lambda failing every time.
    urllib.parse.unquote_plus returns a str, not bytes.
    
    Fixes a bug introduced in #1441.
    
    Tested: deployed to Staging, built Router, checked that the lambda runs
    and copies the file from s3://govuk-staging-artefact to
    s3://govuk-staging-artefact as expected.

--- terraform/lambda/ArtefactSync/main.py
+++ terraform/lambda/ArtefactSync/main.py
@@ -9,7 +9,7 @@ def lambda_handler(event, context):
     sns_message = ast.literal_eval(event['Records'][0]['Sns']['Message'])
     target_bucket = context.function_name
     source_bucket = str(sns_message['Records'][0]['s3']['bucket']['name'])
-    key = str(urllib.parse.unquote_plus(sns_message['Records'][0]['s3']['object']['key']).decode('utf8'))
+    key = urllib.parse.unquote_plus(sns_message['Records'][0]['s3']['object']['key'])
     copy_source = {'Bucket':source_bucket, 'Key':key}
     print("Copying %s from bucket %s to bucket %s ..." % (key, source_bucket, target_bucket))
     s3.copy_object(Bucket=target_bucket, Key=key, CopySource=copy_source)
