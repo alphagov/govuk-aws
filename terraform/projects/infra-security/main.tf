3c23bad92bd8c9e46ec28948323229d634d813de
Vitalie Mogoreanu <vitalie.mogoreanu@digital.cabinet-office.gov.uk>
2021-08-13 15:38:59 +0100

    pass "govuk-datascienceusers" explicitly

--- terraform/projects/infra-security/main.tf
+++ terraform/projects/infra-security/main.tf
@@ -232,7 +232,7 @@ resource "aws_iam_policy" "pass_step_function" {
 }
 
 resource "aws_iam_role_policy_attachment" "datascience_pass_step_function" {
-  role       = module.role_datascienceuser.role_arn
+  role       = "govuk-datascienceusers"
   policy_arn = aws_iam_policy.pass_step_function.arn
 }
 
