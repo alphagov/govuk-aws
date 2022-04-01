e1d285430946e0bc2b53f2597470b1112feba83d
Roch Trinqué <roch.trinque@digital.cabinet-office.gov.uk>
2022-04-01 17:07:45 +0100

    Remove static public LB

--- terraform/projects/infra-public-services/main.tf
+++ terraform/projects/infra-public-services/main.tf
@@ -241,16 +241,6 @@ variable "sidekiq_monitoring_public_service_names" {
   default = []
 }
 
-variable "static_public_service_names" {
-  type    = "list"
-  default = []
-}
-
-variable "support_api_public_service_names" {
-  type    = "list"
-  default = []
-}
-
 variable "whitehall_backend_public_service_names" {
   type    = "list"
   default = []
@@ -2078,40 +2068,6 @@ resource "aws_route53_record" "search_internal_service_cnames" {
 # Static
 #
 
-module "static_public_lb" {
-  source                                     = "../../modules/aws/lb"
-  name                                       = "${var.stackname}-static-public"
-  internal                                   = false
-  vpc_id                                     = "${data.terraform_remote_state.infra_vpc.vpc_id}"
-  access_logs_bucket_name                    = "${data.terraform_remote_state.infra_monitoring.aws_logging_bucket_id}"
-  access_logs_bucket_prefix                  = "elb/${var.stackname}-static-public-elb"
-  listener_certificate_domain_name           = "${var.elb_public_certname}"
-  listener_secondary_certificate_domain_name = "${var.elb_public_secondary_certname}"
-
-  listener_action = {
-    "HTTPS:443" = "HTTP:80"
-  }
-
-  target_group_health_check_path = "/_healthcheck-ready_static"
-  subnets                        = ["${data.terraform_remote_state.infra_networking.public_subnet_ids}"]
-  security_groups                = ["${data.terraform_remote_state.infra_security_groups.sg_static_carrenza_alb_id}"]
-  alarm_actions                  = ["${data.terraform_remote_state.infra_monitoring.sns_topic_cloudwatch_alarms_arn}"]
-  default_tags                   = "${map("Project", var.stackname, "aws_migration", "static", "aws_environment", var.aws_environment)}"
-}
-
-resource "aws_route53_record" "static_public_service_names" {
-  count   = "${length(var.static_public_service_names)}"
-  zone_id = "${data.terraform_remote_state.infra_root_dns_zones.external_root_zone_id}"
-  name    = "${element(var.static_public_service_names, count.index)}.${data.terraform_remote_state.infra_root_dns_zones.external_root_domain_name}"
-  type    = "A"
-
-  alias {
-    name                   = "${module.static_public_lb.lb_dns_name}"
-    zone_id                = "${module.static_public_lb.lb_zone_id}"
-    evaluate_target_health = true
-  }
-}
-
 data "aws_autoscaling_groups" "static" {
   filter {
     name   = "key"
@@ -2122,12 +2078,6 @@ data "aws_autoscaling_groups" "static" {
     name   = "value"
     values = ["blue-frontend"]
   }
-}
-
-resource "aws_autoscaling_attachment" "static_asg_attachment_alb" {
-  count                  = "${length(data.aws_autoscaling_groups.static.names) > 0 ? 1 : 0}"
-  autoscaling_group_name = "${element(data.aws_autoscaling_groups.static.names, 0)}"
-  alb_target_group_arn   = "${element(module.static_public_lb.target_group_arns, 0)}"
 }
 
 # support-api
