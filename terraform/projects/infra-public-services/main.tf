d7bd2c27fc240472f4d7383ffa244af10ab902bf
Roch Trinqué <roch.trinque@digital.cabinet-office.gov.uk>
2022-04-04 12:47:31 +0100

    Remove feedback public LB

--- terraform/projects/infra-public-services/main.tf
+++ terraform/projects/infra-public-services/main.tf
@@ -171,11 +171,6 @@ variable "email_alert_api_public_service_names" {
   default = []
 }
 
-variable "feedback_public_service_names" {
-  type    = "list"
-  default = []
-}
-
 variable "graphite_public_service_names" {
   type    = "list"
   default = []
@@ -1323,44 +1318,6 @@ resource "aws_route53_record" "email_alert_api_internal_service_names" {
   ttl     = "300"
 }
 
-#
-# feedback
-#
-
-module "feedback_public_lb" {
-  source                                     = "../../modules/aws/lb"
-  name                                       = "${var.stackname}-feedback-public"
-  internal                                   = true
-  vpc_id                                     = "${data.terraform_remote_state.infra_vpc.vpc_id}"
-  access_logs_bucket_name                    = "${data.terraform_remote_state.infra_monitoring.aws_logging_bucket_id}"
-  access_logs_bucket_prefix                  = "elb/${var.stackname}-feedback-public-elb"
-  listener_certificate_domain_name           = "${var.elb_public_certname}"
-  listener_secondary_certificate_domain_name = "${var.elb_public_secondary_certname}"
-
-  listener_action = {
-    "HTTPS:443" = "HTTP:80"
-  }
-
-  target_group_health_check_path = "/_healthcheck-ready_feedback"
-  subnets                        = ["${data.terraform_remote_state.infra_networking.private_subnet_ids}"]
-  security_groups                = ["${data.terraform_remote_state.infra_security_groups.sg_feedback_elb_id}"]
-  alarm_actions                  = ["${data.terraform_remote_state.infra_monitoring.sns_topic_cloudwatch_alarms_arn}"]
-  default_tags                   = "${map("Project", var.stackname, "aws_migration", "feedback", "aws_environment", var.aws_environment)}"
-}
-
-resource "aws_route53_record" "feedback_public_service_names" {
-  count   = "${length(var.feedback_public_service_names)}"
-  zone_id = "${data.terraform_remote_state.infra_root_dns_zones.external_root_zone_id}"
-  name    = "${element(var.feedback_public_service_names, count.index)}.${data.terraform_remote_state.infra_root_dns_zones.external_root_domain_name}"
-  type    = "A"
-
-  alias {
-    name                   = "${module.feedback_public_lb.lb_dns_name}"
-    zone_id                = "${module.feedback_public_lb.lb_zone_id}"
-    evaluate_target_health = true
-  }
-}
-
 data "aws_autoscaling_groups" "frontend" {
   filter {
     name   = "key"
@@ -1371,12 +1328,6 @@ data "aws_autoscaling_groups" "frontend" {
     name   = "value"
     values = ["blue-frontend"]
   }
-}
-
-resource "aws_autoscaling_attachment" "frontend_asg_attachment_alb" {
-  count                  = "${length(data.aws_autoscaling_groups.frontend.names) > 0 ? 1 : 0}"
-  autoscaling_group_name = "${element(data.aws_autoscaling_groups.frontend.names, 0)}"
-  alb_target_group_arn   = "${element(module.feedback_public_lb.target_group_arns, 0)}"
 }
 
 #
