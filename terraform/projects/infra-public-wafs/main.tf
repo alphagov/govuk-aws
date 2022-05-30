ccad6fdb68eba16c4815d888c244aa97183dd5e9
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2022-05-30 16:29:06 +0100

    Add boilerplate for new WAFs project
    This was inspired by the set up for [govuk-rds](https://github.com/alphagov/govuk-aws/tree/main/terraform/projects/app-govuk-rds)
    which seems to be one of the few projects to uses a Terraform version > 1.
    
    This project will allow us to remove some config from the infra-public-services
    project (which is quite large), but should also more rapid deployment of
    WAF changes should they be necessary.

---
+++ terraform/projects/infra-public-wafs/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "s3" {}
+  required_version = "= 1.1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
