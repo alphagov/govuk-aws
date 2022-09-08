da0b9bdeda49bde15d2104c6c622244e431f07b6
fredericfran-gds <frederic.francois@digital.cabinet-office.gov.uk>
2022-09-08 14:59:11 +0100

    temp change to be able to delete

--- jenkins.sh
+++ jenkins.sh
@@ -50,7 +50,6 @@ git clone --single-branch --branch "$GOVUK_AWS_DATA_BRANCH" git@github.com:alpha
 case $COMMAND in
   'apply') EXTRA='-auto-approve';;
   'plan (destroy)') COMMAND='plan'; EXTRA='-detailed-exitcode -destroy';;
-  'destroy') EXTRA='-force';;
   'plan') EXTRA='-detailed-exitcode';;
 esac
 
