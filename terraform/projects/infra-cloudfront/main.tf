127c24320783e4b8e678b039682c8b089e60223f
Roch Trinqué <roch.trinque@digital.cabinet-office.gov.uk>
2022-11-17 16:34:09 +0000

    Disable caching for notify alerts

--- terraform/projects/infra-cloudfront/main.tf
+++ terraform/projects/infra-cloudfront/main.tf
@@ -187,7 +187,7 @@ resource "aws_cloudfront_distribution" "www_distribution" {
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = "notify alerts"
     compress                 = "false"
-    cache_policy_id          = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+    cache_policy_id          = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
     viewer_protocol_policy = "redirect-to-https"
   }
 
@@ -197,7 +197,7 @@ resource "aws_cloudfront_distribution" "www_distribution" {
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = "notify alerts"
     compress                 = "false"
-    cache_policy_id          = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+    cache_policy_id          = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
     viewer_protocol_policy = "redirect-to-https"
   }
  
