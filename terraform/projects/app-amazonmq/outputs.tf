7781f8feade9d883ec6cb47a6dc583e9d29ad5b7
Al Davidson <apdavidson@gmail.com>
2022-11-28 23:17:33 +0000

    add descriptions for app-amazonmq output variables

--- terraform/projects/app-amazonmq/outputs.tf
+++ terraform/projects/app-amazonmq/outputs.tf
@@ -1,11 +1,14 @@
 output "broker_id" {
-  value = aws_mq_broker.publishing_amazonmq.id
+  description = "AWS-generated unique identifier for the broker"
+  value       = aws_mq_broker.publishing_amazonmq.id
 }
 
 output "console_url" {
-  value = aws_mq_broker.publishing_amazonmq.instances.0.console_url
+  description = "URL of the RabbitMQ web management UI"
+  value       = aws_mq_broker.publishing_amazonmq.instances.0.console_url
 }
 
 output "endpoint" {
-  value = aws_mq_broker.publishing_amazonmq.instances.0.endpoints.0
+  description = "AMQP URL for connecting a client app to the broker"
+  value       = aws_mq_broker.publishing_amazonmq.instances.0.endpoints.0
 }
