bc93718e4326cbf64d831cd667309d240d28d7fe
Al Davidson <apdavidson@gmail.com>
2022-12-02 16:35:46 +0000

    create data for template params

--- terraform/projects/app-publishing-amazonmq/main.tf
+++ terraform/projects/app-publishing-amazonmq/main.tf
@@ -108,11 +108,18 @@ resource "aws_route53_record" "publishing_amazonmq_internal_root_domain_name" {
 
 # --------------------------------------------------------------
 # POST full RabbitMQ config to the management API
+data "template_params" {
+  type = map
+  value = {
+    publishing_amazonmq_passwords   = var.publishing_amazonmq_passwords
+    publishing_amazonmq_broker_name = var.publishing_amazonmq_broker_name
+  }
+}
 
 # Write the decrypted definitions from govuk-aws-data to a local file
 resource "local_sensitive_file" "amazonmq_rabbitmq_definitions" {
   filename = "/tmp/amazonmq_rabbitmq_definitions-${timestamp()}.json"
-  content  = templatefile("${path.cwd}/publishing-rabbitmq-schema.json.tpl", var)
+  content  = templatefile("${path.cwd}/publishing-rabbitmq-schema.json.tpl", data.template_params)
 }
 
 # POST that definitions file to the Rabbitmq HTTP API (see https://pulse.mozilla.org/api/index.html)
