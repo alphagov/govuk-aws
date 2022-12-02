ba28ac5f76057fae75cce5760e3c284a55e285e2
Al Davidson <apdavidson@gmail.com>
2022-12-02 10:53:28 +0000

    improve -auto-approve comment

--- jenkins.sh
+++ jenkins.sh
@@ -50,7 +50,9 @@ git clone --single-branch --branch "$GOVUK_AWS_DATA_BRANCH" git@github.com:alpha
 case $COMMAND in
   'apply') EXTRA='-auto-approve';;
   'plan (destroy)') COMMAND='plan'; EXTRA='-detailed-exitcode -destroy';;
-  # The -force flag has been deprecated since TF v1.0, supplying it causes errors in TF v1.0+ 
+  # This flag must be -auto-approve for terraform v1.0+
+  # TODO: either also support -force for terraform v0.x, or update remaining
+  #       projects that require terraform v0.x 
   'destroy') EXTRA='-auto-approve';;
   'plan') EXTRA='-detailed-exitcode';;
 esac
