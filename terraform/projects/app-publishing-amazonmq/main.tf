7010924c94e66adf9d214c70971a480ad50e7d1f
Al Davidson <apdavidson@gmail.com>
2022-12-02 14:37:44 +0000

    doc fix

--- terraform/projects/app-publishing-amazonmq/main.tf
+++ terraform/projects/app-publishing-amazonmq/main.tf
@@ -18,7 +18,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.4.5"
+      version = "~> 4.45.0"
     }
     local = {
       version = "~> 2.2.3"
