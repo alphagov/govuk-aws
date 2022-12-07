93051187c2dcbd84e37b5c930211bf2102f635c6
Roch Trinqué <roch.trinque@digital.cabinet-office.gov.uk>
2022-12-07 16:55:11 +0000

    Add dedicated domain name for the api gateway

--- terraform/projects/infra-csp-reporter/api_gateway.tf
+++ terraform/projects/infra-csp-reporter/api_gateway.tf
@@ -26,3 +26,25 @@ resource "aws_apigatewayv2_stage" "default" {
   name        = "$default"
   auto_deploy = true
 }
+
+resource "aws_apigatewayv2_domain_name" "csp_report" {
+  domain_name = "csp-report.${data.terraform_remote_state.infra_root_dns_zones.outputs.external_root_domain_name}"
+
+  domain_name_configuration {
+    certificate_arn = "${data.terraform_remote_state.infra_certificates.outputs.external_certificate_arn}"
+    endpoint_type   = "REGIONAL"
+    security_policy = "TLS_1_2"
+  }
+}
+
+resource "aws_route53_record" "csp_report" {
+  name    = aws_apigatewayv2_domain_name.csp_report.domain_name
+  type    = "A"
+  zone_id = "${data.terraform_remote_state.infra_root_dns_zones.outputs.external_root_zone_id}"
+
+  alias {
+    name                   = aws_apigatewayv2_domain_name.csp_report.domain_name_configuration[0].target_domain_name
+    zone_id                = aws_apigatewayv2_domain_name.csp_report.domain_name_configuration[0].hosted_zone_id
+    evaluate_target_health = false
+  }
+}
