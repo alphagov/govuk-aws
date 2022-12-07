f2fbd6cc47d2ed6c956903c7b2d2f8ef3a7ef8a4
Kevin Dew <kevindew@me.com>
2022-12-07 12:14:08 +0000

    Glue configuration for reading S3 data
    This configures Glue to provide an Athena database to query CSP reports.
    The columns are based on data that the CspReportsToFirehose lambda
    produces, with partitions based on folder structure.
    
    It expects ORC [1] as a file format for the files which is a columnar
    data structure that is effective for Athena queries.
    
    As we are expecting files to be in a partition directory structure, we
    need a glue crawler to discover the partitions. This crawler has been
    configured to not change the schema based on the data it finds and to
    delete references to partitions that are missing.
    
    I've set glue to run once an hour as we need it to run after a partition
    is created but we're not clear on exactly when a partition may be
    created (as CSP reports may be rather adhoc) thus this will mean the
    data queried is perhaps delayed by an hour at times.
    
    [1]: https://orc.apache.org/

---
+++ terraform/projects/infra-csp-reporter/glue.tf
@@ -0,0 +1,199 @@
+resource "aws_glue_catalog_database" "csp_reports" {
+  name        = "csp_reports"
+  description = "Used to browse the Content Security Policy violations"
+}
+
+resource "aws_iam_role" "glue_role" {
+  name = "govuk-${var.aws_environment}-csp-reports-glue-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "glue.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "glue_service" {
+  role       = aws_iam_role.glue_role.id
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
+
+resource "aws_iam_role_policy" "glue_policy" {
+  name = "govuk-${var.aws_environment}-csp-reports-glue-policy"
+  role = aws_iam_role.glue_role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:*"
+      ],
+      "Resource": [
+        "arn:aws:s3:::${aws_s3_bucket.csp_reports.id}",
+        "arn:aws:s3:::${aws_s3_bucket.csp_reports.id}/*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_glue_crawler" "csp_reports" {
+  name          = "Content Security Policy reports"
+  description   = "Crawls the CSP logs for allowing Athena querying"
+  database_name = aws_glue_catalog_database.csp_reports.name
+  role          = aws_iam_role.glue_role.name
+  schedule      = "cron(15 * * * ? *)" # Run every hour as we're not sure how frequently we'll get reports
+
+  tags = {
+    aws_environment = var.aws_environment
+    project         = local.project_name
+  }
+
+  s3_target {
+    path = "s3://${aws_s3_bucket.csp_reports.bucket}/reports"
+  }
+
+  schema_change_policy {
+    delete_behavior = "DELETE_FROM_DATABASE"
+    update_behavior = "LOG"
+  }
+
+  configuration = <<EOF
+{
+  "Version": 1.0,
+  "CrawlerOutput": {
+    "Partitions": {
+      "AddOrUpdateBehavior": "InheritFromTable"
+    }
+  }
+}
+EOF
+}
+
+resource "aws_glue_catalog_table" "reports" {
+  name          = "reports"
+  description   = "Allows access to CSP reports"
+  database_name = aws_glue_catalog_database.csp_reports.name
+  table_type    = "EXTERNAL_TABLE"
+
+  storage_descriptor {
+    compressed    = true
+    location      = "s3://${aws_s3_bucket.csp_reports.bucket}/reports/"
+    input_format  = "org.apache.hadoop.hive.ql.io.orc.OrcInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.orc.OrcOutputFormat"
+
+    ser_de_info {
+      serialization_library = "org.apache.hadoop.hive.ql.io.orc.OrcSerde"
+    }
+
+    // These columns correlate with the CspReportsToFirehose Lambda
+    columns {
+      name    = "time"
+      type    = "timestamp"
+      comment = "Time report was received"
+    }
+
+    columns {
+      name    = "document_uri"
+      type    = "string"
+      comment = "URI of the document that initiated the CSP report"
+    }
+
+    columns {
+      name    = "referrer"
+      type    = "string"
+      comment = "Referrer to the document"
+    }
+
+    columns {
+      name    = "blocked_uri"
+      type    = "string"
+      comment = "URI of the resource that violates the CSP policy"
+    }
+
+    columns {
+      name    = "effective_directive"
+      type    = "string"
+      comment = "The CSP directive that causes the violation"
+    }
+
+    columns {
+      name    = "violated_directive"
+      type    = "string"
+      comment = "The CSP directive that causes the violation, typically an alias of effective_directive"
+    }
+
+    columns {
+      name    = "disposition"
+      type    = "string"
+      comment = "Whether this was a report or a block"
+    }
+
+    columns {
+      name    = "sample"
+      type    = "string"
+      comment = "Sample code from the violation"
+    }
+
+    columns {
+      name    = "line_number"
+      type    = "int"
+      comment = "The line number from the document that triggered the report"
+    }
+
+    columns {
+      name    = "status_code"
+      type    = "int"
+      comment = "HTTP status code of the violation resource"
+    }
+
+    columns {
+      name    = "source_ip"
+      type    = "string"
+      comment = "IP of the device that requested the resource"
+    }
+
+    columns {
+      name    = "user_agent"
+      type    = "string"
+      comment = "User agent from the device that requested the resource"
+    }
+  }
+
+  // these correspond to directory ordering of:
+  // /year=YYYY/month=MM/date=DD/file.log.gz
+  partition_keys {
+    name = "year"
+    type = "int"
+  }
+
+  partition_keys {
+    name = "month"
+    type = "int"
+  }
+
+  partition_keys {
+    name = "date"
+    type = "int"
+  }
+
+  parameters = {
+    classification  = "orc"
+    compressionType = "snappy"
+    typeOfDate      = "file"
+  }
+}
