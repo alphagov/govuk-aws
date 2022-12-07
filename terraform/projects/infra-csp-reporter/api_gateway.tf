8b9f69ee64f3fed6da06cf230178cfd69afac203
Roch Trinqué <roch.trinque@digital.cabinet-office.gov.uk>
2022-12-07 12:31:04 +0000

    Add API gateway

--- terraform/projects/infra-csp-reporter/api_gateway.tf
+++ terraform/projects/infra-csp-reporter/api_gateway.tf
@@ -1,1 +1,26 @@
-# API Gateway - lambda expects payload version 2
+resource "aws_apigatewayv2_api" "csp_report" {
+  name          = "CSP report"
+  protocol_type = "HTTP"
+  description   = "Receive CSP reports"
+}
+
+resource "aws_apigatewayv2_integration" "csp_report" {
+  api_id           = aws_apigatewayv2_api.csp_report.id
+  integration_type = "AWS_PROXY"
+
+  description               = "Send CSP reports to firehose"
+  integration_method        = "POST"
+  integration_uri           = aws_lambda_function.lambda.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "csp_report" {
+  api_id    = aws_apigatewayv2_api.csp_report.id
+  route_key = "POST /csp_report"
+
+  target = "integrations/${aws_apigatewayv2_integration.csp_report.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id = aws_apigatewayv2_api.csp_report.id
+  name   = "$default"
+}
