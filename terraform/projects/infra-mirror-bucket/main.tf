01ef070b8d3cb8a0ba18ddcf491b374b29f674f2
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2023-01-19 10:05:53 +0000

    Temporarily bump NodeJS runtime to v12
    The `apply` is failing because the version of Node we're on is no
    longer supported:
    
    ```
    Error: Error modifying Lambda Function Configuration url_rewrite: InvalidParameterValueException: The runtime parameter of nodejs10.x is no longer supported for creating or updating AWS Lambda functions. We recommend you use the new runtime (nodejs16.x) while creating or updating functions.
    {
      Message_: "The runtime parameter of nodejs10.x is no longer supported for creating or updating AWS Lambda functions. We recommend you use the new runtime (nodejs16.x) while creating or updating functions.",
      Type: "User"
    }
    
      on main.tf line 618, in resource "aws_lambda_function" "url_rewrite":
     618: resource "aws_lambda_function" "url_rewrite" {
    ```
    
    This is an ironic thing to fail on, as the entire point of us
    upgrading the Terraform is to upgrade the Node version, but we're
    unable to jump straight to the v1.1 version of Terraform required
    to support Node v16. So this is an attempt to fix the immediate
    error we're seeing, even if we're planning to bump to v16 very
    shortly.

--- terraform/projects/infra-mirror-bucket/main.tf
+++ terraform/projects/infra-mirror-bucket/main.tf
@@ -622,6 +622,6 @@ resource "aws_lambda_function" "url_rewrite" {
   function_name = "url_rewrite"
   role          = aws_iam_role.basic_lambda_role.arn
   handler       = "index.handler"
-  runtime       = "nodejs10.x"
+  runtime       = "nodejs12.x"
   provider      = aws.aws_cloudfront_certificate
 }
