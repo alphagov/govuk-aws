3785578a6633daedef284e4a337d9b82e98a7e14
Roch Trinqué <roch.trinque@digital.cabinet-office.gov.uk>
2023-01-23 00:52:48 +0000

    Add Assets distribution

--- terraform/projects/infra-cloudfront/main.tf
+++ terraform/projects/infra-cloudfront/main.tf
@@ -266,5 +266,73 @@ ordered_cache_behavior {
     Project         = "${var.stackname}"
     aws_environment = "${var.aws_environment}"
   }
+
+resource "aws_cloudfront_distribution" "assets_distribution" {
+  count = "${var.cloudfront_create}"
+
+  origin {
+    domain_name = "${aws_s3_bucket.govuk-mirror.bucket_domain_name}"
+    origin_id   = "S3-govuk-${var.aws_environment}-mirror/assets.publishing.service.gov.uk"
+    origin_path = "/assets.publishing.service.gov.uk"
+
+    s3_origin_config {
+      origin_access_identity = "${aws_cloudfront_origin_access_identity.mirror_access_identity.cloudfront_access_identity_path}"
+    }
+  }
+
+  enabled         = "${var.cloudfront_enable}"
+  is_ipv6_enabled = true
+  comment         = "Assets"
+
+  logging_config {
+    include_cookies = false
+    bucket          = "${data.terraform_remote_state.infra_monitoring.aws_logging_bucket_id}.s3.amazonaws.com"
+    prefix          = "cloudfront/"
+  }
+
+  aliases = ["${var.cloudfront_assets_distribution_aliases}"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-govuk-${var.aws_environment}-mirror/assets.publishing.service.gov.uk"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+
+      headers = ["Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"]
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+  }
+
+  price_class = "PriceClass_All"
+
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = "${data.aws_acm_certificate.assets.arn}"
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.1_2016"
+  }
+
+  tags = {
+    Project         = "${var.stackname}"
+    aws_environment = "${var.aws_environment}"
+  }
+}
+
 }
 
