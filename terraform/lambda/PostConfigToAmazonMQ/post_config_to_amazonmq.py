f50f5f91a712589bd4f79b0014c25cd6a10b6db8
Al Davidson <apdavidson@gmail.com>
2023-01-26 22:03:41 +0000

    try posting json as file

--- terraform/lambda/PostConfigToAmazonMQ/post_config_to_amazonmq.py
+++ terraform/lambda/PostConfigToAmazonMQ/post_config_to_amazonmq.py
@@ -5,23 +5,22 @@ import logging
 
 from requests.auth import HTTPBasicAuth
 
+
 def lambda_handler(event, context):
-    # Initialize you log configuration using the base class
-    logging.basicConfig(level = logging.INFO)
+    raw_json = base64.b64decode(event['json_b64'])
 
-    # Retrieve the logger instance
-    logger = logging.getLogger()
+    # write to file
+    f = open('/tmp/json-config.json', 'wb')
+    f.write(raw_json)
+    f.close()
 
-    logger.info("event = %s", event)
+    print("raw_json = %s", raw_json)
 
-    raw_json = print(base64.b64decode(event['json_b64']))
-
-    logger.info("raw_json = %s", raw_json)
-
-    logger.info('Posting...')
+    print('Posting... /tmp/json-config.json to %s', event['url'])
     headers = {'Content-type': 'application/json'}
-    r = requests.post(event['url'], data=raw_json, auth=HTTPBasicAuth(event['username'], event['password']), headers=headers)
-    logger.info('Posted - status_code = %s', r.status_code)
+    files = {'file': open('/tmp/json-config.json','rb')}
+    r = requests.post(event['url'], files=files, auth=HTTPBasicAuth(event['username'], event['password']), headers=headers)
+    print('Posted - status_code = %s', r.status_code)
 
     return {
         'statusCode': r.status_code,
