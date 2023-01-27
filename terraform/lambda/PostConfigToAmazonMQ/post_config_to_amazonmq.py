97ad6b82b6a3266223352a76ed357eb7c469daf9
Al Davidson <apdavidson@gmail.com>
2023-01-27 18:50:12 +0000

    regex validation that url is an AmazonMQ url

--- terraform/lambda/PostConfigToAmazonMQ/post_config_to_amazonmq.py
+++ terraform/lambda/PostConfigToAmazonMQ/post_config_to_amazonmq.py
@@ -1,12 +1,19 @@
 import base64
 import json
 import requests
+import re
 
 from requests.auth import HTTPBasicAuth
+
+def raise_if_not_an_amazonmq_url(url):
+    if re.fullmatch('.*\.mq\.[a-z0-9_-]+\.amazonaws\.com/.*', url) == None:
+        raise ValueError(f'url failed validation: {url}')
 
 def lambda_handler(event, context):
     raw_json = base64.b64decode(event['json_b64'])
     json_str = raw_json.decode('ascii')
+
+    raise_if_not_an_amazonmq_url(event['url'])
 
     print('Posting json to ', event['url'])
     headers = {'Content-type': 'application/json'}
