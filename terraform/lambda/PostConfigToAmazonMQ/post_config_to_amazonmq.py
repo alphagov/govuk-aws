f21b07b577bb5758d8c004279d8468278f3ad258
Al Davidson <apdavidson@gmail.com>
2023-01-30 11:07:28 +0000

    remove reliance on requests, and rewrite using urllib

--- terraform/lambda/PostConfigToAmazonMQ/post_config_to_amazonmq.py
+++ terraform/lambda/PostConfigToAmazonMQ/post_config_to_amazonmq.py
@@ -1,28 +1,40 @@
 import base64
-import json
-import requests
+
 import re
 
-from requests.auth import HTTPBasicAuth
+from urllib import request
 
 def raise_if_not_an_amazonmq_url(url):
-    if re.fullmatch('.*\.mq\.[a-z0-9_-]+\.amazonaws\.com/.*', url) == None:
-        raise ValueError(f'url failed validation: {url}')
+    if re.fullmatch(r'.*\.mq\.[a-z0-9_-]+\.amazonaws\.com/.*', url) is None:
+        raise ValueError(f'Not a valid AmazonMQ URL: {url}')
+
+def auth_digest(username, password):
+    credentials = "%s:%s" % (username, password)
+    return base64.b64encode(credentials.encode('UTF8'))
+
+def add_headers(req, event):
+    auth_header_value = b'Basic ' + auth_digest(event['username'], event['password'])
+    req.add_header('Authorization', auth_header_value)
+
+    req.add_header('Content-type', 'application/json')
+
+    return req
 
 def lambda_handler(event, context):
-    raw_json = base64.b64decode(event['json_b64'])
-    json_str = raw_json.decode('ascii')
-
     raise_if_not_an_amazonmq_url(event['url'])
 
+    raw_json = base64.b64decode(event['json_b64'])
+    req = request.Request(event['url'], data=raw_json, method='POST', unverifiable=True)
+    add_headers(req, event)
+
     print('Posting json to ', event['url'])
-    headers = {'Content-type': 'application/json'}
-    r = requests.post(event['url'], data=json_str, auth=HTTPBasicAuth(event['username'], event['password']), headers=headers)
-    
-    print('Posted - status_code = ', r.status_code)
-    print('Response body: (only present if an error occurred)', r.text)
+    resp = request.urlopen(req)
+
+    print('Posted - status_code = ', resp.status)
+    response_body = resp.read()
+    print('Response body: (only present if an error occurred)', response_body)
 
     return {
-        'statusCode': r.status_code,
-        'body': r.content
+        'statusCode': resp.status,
+        'body': response_body
     }
