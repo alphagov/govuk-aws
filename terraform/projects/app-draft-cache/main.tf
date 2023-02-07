ae21656da123bfadb7e9cb506be80bb7df1ab6b6
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2023-02-07 17:30:44 +0000

    Fix "Missing resource instance key" error
      on main.tf line 245, in resource "aws_route53_record" "draft-cache_external_service_record":
     245:     name                   = aws_elb.draft-cache_external_elb.dns_name
    
    Because aws_elb.draft-cache_external_elb has "count" set, its attributes must
    be accessed on specific instances.
    
    For example, to correlate with indices of a referring resource, use:
        aws_elb.draft-cache_external_elb[count.index]

--- terraform/projects/app-draft-cache/main.tf
+++ terraform/projects/app-draft-cache/main.tf
@@ -242,8 +242,8 @@ resource "aws_route53_record" "draft-cache_external_service_record" {
   type    = "A"
 
   alias {
-    name                   = aws_elb.draft-cache_external_elb.dns_name
-    zone_id                = aws_elb.draft-cache_external_elb.zone_id
+    name                   = aws_elb.draft-cache_external_elb.0.dns_name
+    zone_id                = aws_elb.draft-cache_external_elb.0.zone_id
     evaluate_target_health = true
   }
 }
