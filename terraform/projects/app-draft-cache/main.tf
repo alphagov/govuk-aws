4d91b3afea0a27db38e00dab2dff98851cee32f9
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2023-02-07 11:58:12 +0000

    Reformat app-draft-cache after 0.12 upgrade

--- terraform/projects/app-draft-cache/main.tf
+++ terraform/projects/app-draft-cache/main.tf
@@ -4,72 +4,72 @@
 * Draft Cache servers
 */
 variable "aws_region" {
-  type        = "string"
+  type        = string
   description = "AWS region"
   default     = "eu-west-1"
 }
 
 variable "stackname" {
-  type        = "string"
+  type        = string
   description = "Stackname"
 }
 
 variable "aws_environment" {
-  type        = "string"
+  type        = string
   description = "AWS Environment"
 }
 
 variable "instance_ami_filter_name" {
-  type        = "string"
+  type        = string
   description = "Name to use to find AMI images"
   default     = ""
 }
 
 variable "elb_internal_certname" {
-  type        = "string"
+  type        = string
   description = "The ACM cert domain name to find the ARN of"
 }
 
 variable "elb_external_certname" {
-  type        = "string"
+  type        = string
   description = "The ACM cert domain name to find the ARN of"
 }
 
 variable "app_service_records" {
-  type        = "list"
+  type        = list(string)
   description = "List of application service names that get traffic via this loadbalancer"
   default     = []
 }
 
 variable "asg_size" {
-  type        = "string"
+  type        = string
   description = "The autoscaling groups desired/max/min capacity"
   default     = "2"
 }
 
 variable "instance_type" {
-  type        = "string"
+  type        = string
   description = "Instance type used for EC2 resources"
   default     = "t2.medium"
 }
 
 variable "external_zone_name" {
-  type        = "string"
+  type        = string
   description = "The name of the Route53 zone that contains external records"
 }
 
 variable "external_domain_name" {
-  type        = "string"
+  type        = string
   description = "The domain name of the external DNS records, it could be different from the zone name"
 }
 
 variable "internal_zone_name" {
-  type        = "string"
+  type        = string
   description = "The name of the Route53 zone that contains internal records"
 }
 
 variable "internal_domain_name" {
-  type        = "string"
+  type        = string
   description = "The domain name of the internal DNS records, it could be different from the zone name"
 }
 
@@ -81,32 +81,32 @@ variable "create_external_elb" {
 # Resources
 # --------------------------------------------------------------
 terraform {
-  backend          "s3"             {}
+  backend "s3" {}
   required_version = "= 0.12.30"
 }
 
 provider "aws" {
-  region  = "${var.aws_region}"
+  region  = var.aws_region
   version = "2.46.0"
 }
 
 data "aws_route53_zone" "external" {
-  name         = "${var.external_zone_name}"
+  name         = var.external_zone_name
   private_zone = false
 }
 
 data "aws_route53_zone" "internal" {
-  name         = "${var.internal_zone_name}"
+  name         = var.internal_zone_name
   private_zone = true
 }
 
 data "aws_acm_certificate" "elb_internal_cert" {
-  domain   = "${var.elb_internal_certname}"
+  domain   = var.elb_internal_certname
   statuses = ["ISSUED"]
 }
 
 data "aws_acm_certificate" "elb_external_cert" {
-  domain   = "${var.elb_external_certname}"
+  domain   = var.elb_external_certname
   statuses = ["ISSUED"]
 }
 
@@ -117,7 +117,7 @@ resource "aws_elb" "draft-cache_elb" {
   internal        = "true"
 
   access_logs {
-    bucket        = "${data.terraform_remote_state.infra_monitoring.aws_logging_bucket_id}"
+    bucket        = data.terraform_remote_state.infra_monitoring.aws_logging_bucket_id
     bucket_prefix = "elb/${var.stackname}-draft-cache-internal-elb"
     interval      = 60
   }
@@ -128,7 +128,7 @@ resource "aws_elb" "draft-cache_elb" {
     lb_port           = 443
     lb_protocol       = "https"
 
-    ssl_certificate_id = "${data.aws_acm_certificate.elb_internal_cert.arn}"
+    ssl_certificate_id = data.aws_acm_certificate.elb_internal_cert.arn
   }
 
   health_check {
@@ -145,41 +145,41 @@ resource "aws_elb" "draft-cache_elb" {
   connection_draining         = true
   connection_draining_timeout = 400
 
-  tags = "${map("Name", "${var.stackname}-draft-cache", "Project", var.stackname, "aws_environment", var.aws_environment, "aws_migration", "draft_cache")}"
+  tags = map("Name", "${var.stackname}-draft-cache", "Project", var.stackname, "aws_environment", var.aws_environment, "aws_migration", "draft_cache")
 }
 
 resource "aws_route53_record" "draft-cache_service_record" {
-  zone_id = "${data.aws_route53_zone.internal.zone_id}"
+  zone_id = data.aws_route53_zone.internal.zone_id
   name    = "draft-cache.${var.internal_domain_name}"
   type    = "A"
 
   alias {
-    name                   = "${aws_elb.draft-cache_elb.dns_name}"
-    zone_id                = "${aws_elb.draft-cache_elb.zone_id}"
+    name                   = aws_elb.draft-cache_elb.dns_name
+    zone_id                = aws_elb.draft-cache_elb.zone_id
     evaluate_target_health = true
   }
 }
 
 resource "aws_route53_record" "authenticating-proxy_service_record" {
-  zone_id = "${data.aws_route53_zone.internal.zone_id}"
+  zone_id = data.aws_route53_zone.internal.zone_id
   name    = "authenticating-proxy.${var.internal_domain_name}"
   type    = "A"
 
   alias {
-    name                   = "${aws_elb.draft-cache_elb.dns_name}"
-    zone_id                = "${aws_elb.draft-cache_elb.zone_id}"
+    name                   = aws_elb.draft-cache_elb.dns_name
+    zone_id                = aws_elb.draft-cache_elb.zone_id
     evaluate_target_health = true
   }
 }
 
 resource "aws_route53_record" "draft-router-api_internal_record" {
-  zone_id = "${data.aws_route53_zone.internal.zone_id}"
+  zone_id = data.aws_route53_zone.internal.zone_id
   name    = "draft-router-api.${var.internal_domain_name}"
   type    = "A"
 
   alias {
-    name                   = "${aws_elb.draft-cache_elb.dns_name}"
-    zone_id                = "${aws_elb.draft-cache_elb.zone_id}"
+    name                   = aws_elb.draft-cache_elb.dns_name
+    zone_id                = aws_elb.draft-cache_elb.zone_id
     evaluate_target_health = true
   }
 }
@@ -187,7 +187,7 @@ resource "aws_route53_record" "draft-router-api_internal_record" {
 # TODO publicapi is a special set of nginx config that routes /api requests to
 # their relevant apps upstream.
 resource "aws_route53_record" "draft-cache_publicapi_service_record" {
-  zone_id = "${data.aws_route53_zone.internal.zone_id}"
+  zone_id = data.aws_route53_zone.internal.zone_id
   name    = "draft-publicapi.${var.internal_domain_name}"
   type    = "CNAME"
   records = ["draft-cache.${var.internal_domain_name}."]
@@ -195,7 +195,7 @@ resource "aws_route53_record" "draft-cache_publicapi_service_record" {
 }
 
 resource "aws_elb" "draft-cache_external_elb" {
-  count = "${var.create_external_elb}"
+  count = var.create_external_elb
 
   name            = "${var.stackname}-draft-cache-external"
   subnets         = ["${data.terraform_remote_state.infra_networking.public_subnet_ids}"]
@@ -203,7 +203,7 @@ resource "aws_elb" "draft-cache_external_elb" {
   internal        = "false"
 
   access_logs {
-    bucket        = "${data.terraform_remote_state.infra_monitoring.aws_logging_bucket_id}"
+    bucket        = data.terraform_remote_state.infra_monitoring.aws_logging_bucket_id
     bucket_prefix = "elb/${var.stackname}-draft-cache-external-elb"
     interval      = 60
   }
@@ -214,7 +214,7 @@ resource "aws_elb" "draft-cache_external_elb" {
     lb_port           = 443
     lb_protocol       = "https"
 
-    ssl_certificate_id = "${data.aws_acm_certificate.elb_external_cert.arn}"
+    ssl_certificate_id = data.aws_acm_certificate.elb_external_cert.arn
   }
 
   health_check {
@@ -231,26 +231,26 @@ resource "aws_elb" "draft-cache_external_elb" {
   connection_draining         = true
   connection_draining_timeout = 400
 
-  tags = "${map("Name", "${var.stackname}-draft-cache", "Project", var.stackname, "aws_environment", var.aws_environment, "aws_migration", "draft-cache")}"
+  tags = map("Name", "${var.stackname}-draft-cache", "Project", var.stackname, "aws_environment", var.aws_environment, "aws_migration", "draft-cache")
 }
 
 resource "aws_route53_record" "draft-cache_external_service_record" {
-  count = "${var.create_external_elb}"
+  count = var.create_external_elb
 
-  zone_id = "${data.aws_route53_zone.external.zone_id}"
+  zone_id = data.aws_route53_zone.external.zone_id
   name    = "draft-cache.${var.external_domain_name}"
   type    = "A"
 
   alias {
-    name                   = "${aws_elb.draft-cache_external_elb.dns_name}"
-    zone_id                = "${aws_elb.draft-cache_external_elb.zone_id}"
+    name                   = aws_elb.draft-cache_external_elb.dns_name
+    zone_id                = aws_elb.draft-cache_external_elb.zone_id
     evaluate_target_health = true
   }
 }
 
 resource "aws_route53_record" "app_service_records" {
-  count   = "${length(var.app_service_records)}"
-  zone_id = "${data.aws_route53_zone.external.zone_id}"
+  count   = length(var.app_service_records)
+  zone_id = data.aws_route53_zone.external.zone_id
   name    = "${element(var.app_service_records, count.index)}.${var.external_domain_name}"
   type    = "CNAME"
   records = ["draft-cache.${var.external_domain_name}."]
@@ -258,32 +258,32 @@ resource "aws_route53_record" "app_service_records" {
 }
 
 locals {
-  instance_elb_ids_length = "${var.create_external_elb ? 2 : 1}"
-  instance_elb_ids        = "${compact(list(aws_elb.draft-cache_elb.id, join("", aws_elb.draft-cache_external_elb.*.id)))}"
+  instance_elb_ids_length = var.create_external_elb ? 2 : 1
+  instance_elb_ids        = compact(list(aws_elb.draft-cache_elb.id, join("", aws_elb.draft-cache_external_elb.*.id)))
 }
 
 module "draft-cache" {
   source                        = "../../modules/aws/node_group"
   name                          = "${var.stackname}-draft-cache"
-  default_tags                  = "${map("Project", var.stackname, "aws_stackname", var.stackname, "aws_environment", var.aws_environment, "aws_migration", "draft_cache", "aws_hostname", "draft-cache-1")}"
-  instance_subnet_ids           = "${data.terraform_remote_state.infra_networking.private_subnet_ids}"
+  default_tags                  = map("Project", var.stackname, "aws_stackname", var.stackname, "aws_environment", var.aws_environment, "aws_migration", "draft_cache", "aws_hostname", "draft-cache-1")
+  instance_subnet_ids           = data.terraform_remote_state.infra_networking.private_subnet_ids
   instance_security_group_ids   = ["${data.terraform_remote_state.infra_security_groups.sg_draft-cache_id}", "${data.terraform_remote_state.infra_security_groups.sg_management_id}"]
-  instance_type                 = "${var.instance_type}"
-  instance_additional_user_data = "${join("\n", null_resource.user_data.*.triggers.snippet)}"
-  instance_elb_ids_length       = "${local.instance_elb_ids_length}"
+  instance_type                 = var.instance_type
+  instance_additional_user_data = join("\n", null_resource.user_data.*.triggers.snippet)
+  instance_elb_ids_length       = local.instance_elb_ids_length
   instance_elb_ids              = ["${local.instance_elb_ids}"]
-  instance_ami_filter_name      = "${var.instance_ami_filter_name}"
-  asg_max_size                  = "${var.asg_size}"
-  asg_min_size                  = "${var.asg_size}"
-  asg_desired_capacity          = "${var.asg_size}"
-  asg_notification_topic_arn    = "${data.terraform_remote_state.infra_monitoring.sns_topic_autoscaling_group_events_arn}"
+  instance_ami_filter_name      = var.instance_ami_filter_name
+  asg_max_size                  = var.asg_size
+  asg_min_size                  = var.asg_size
+  asg_desired_capacity          = var.asg_size
+  asg_notification_topic_arn    = data.terraform_remote_state.infra_monitoring.sns_topic_autoscaling_group_events_arn
 }
 
 module "alarms-elb-draft-cache-internal" {
   source                         = "../../modules/aws/alarms/elb"
   name_prefix                    = "${var.stackname}-draft-cache-internal"
   alarm_actions                  = ["${data.terraform_remote_state.infra_monitoring.sns_topic_cloudwatch_alarms_arn}"]
-  elb_name                       = "${aws_elb.draft-cache_elb.name}"
+  elb_name                       = aws_elb.draft-cache_elb.name
   httpcode_backend_4xx_threshold = "0"
   httpcode_backend_5xx_threshold = "100"
   httpcode_elb_4xx_threshold     = "100"
@@ -293,20 +293,20 @@ module "alarms-elb-draft-cache-internal" {
 }
 
 locals {
-  elb_httpcode_backend_5xx_threshold = "${var.create_external_elb ? 100 : 0}"
-  elb_httpcode_elb_4xx_threshold     = "${var.create_external_elb ? 100 : 0}"
-  elb_httpcode_elb_5xx_threshold     = "${var.create_external_elb ? 100 : 0}"
+  elb_httpcode_backend_5xx_threshold = var.create_external_elb ? 100 : 0
+  elb_httpcode_elb_4xx_threshold     = var.create_external_elb ? 100 : 0
+  elb_httpcode_elb_5xx_threshold     = var.create_external_elb ? 100 : 0
 }
 
 module "alarms-elb-draft-cache-external" {
   source                         = "../../modules/aws/alarms/elb"
   name_prefix                    = "${var.stackname}-draft-cache-external"
   alarm_actions                  = ["${data.terraform_remote_state.infra_monitoring.sns_topic_cloudwatch_alarms_arn}"]
-  elb_name                       = "${join("", aws_elb.draft-cache_external_elb.*.name)}"
+  elb_name                       = join("", aws_elb.draft-cache_external_elb.*.name)
   httpcode_backend_4xx_threshold = "0"
-  httpcode_backend_5xx_threshold = "${local.elb_httpcode_backend_5xx_threshold}"
-  httpcode_elb_4xx_threshold     = "${local.elb_httpcode_elb_4xx_threshold}"
-  httpcode_elb_5xx_threshold     = "${local.elb_httpcode_elb_4xx_threshold}"
+  httpcode_backend_5xx_threshold = local.elb_httpcode_backend_5xx_threshold
+  httpcode_elb_4xx_threshold     = local.elb_httpcode_elb_4xx_threshold
+  httpcode_elb_5xx_threshold     = local.elb_httpcode_elb_4xx_threshold
   surgequeuelength_threshold     = "0"
   healthyhostcount_threshold     = "0"
 }
@@ -321,34 +321,34 @@ resource "aws_security_group_rule" "authenticating-proxy-rds_ingress_draft_cache
   to_port   = 5432
   protocol  = "tcp"
 
-  security_group_id        = "${data.aws_security_group.authenticating-proxy-rds.0.id}"
-  source_security_group_id = "${data.terraform_remote_state.infra_security_groups.sg_draft-cache_id}"
+  security_group_id        = data.aws_security_group.authenticating-proxy-rds.0.id
+  source_security_group_id = data.terraform_remote_state.infra_security_groups.sg_draft-cache_id
 }
 
 # Outputs
 # --------------------------------------------------------------
 
 output "draft-cache_elb_dns_name" {
-  value       = "${aws_elb.draft-cache_elb.dns_name}"
+  value       = aws_elb.draft-cache_elb.dns_name
   description = "DNS name to access the draft-cache service"
 }
 
 output "draft-cache_external_elb_dns_name" {
-  value       = "${join("", aws_elb.draft-cache_external_elb.*.dns_name)}"
+  value       = join("", aws_elb.draft-cache_external_elb.*.dns_name)
   description = "DNS name to access the draft-cache external service"
 }
 
 output "service_dns_name" {
-  value       = "${aws_route53_record.draft-cache_service_record.fqdn}"
+  value       = aws_route53_record.draft-cache_service_record.fqdn
   description = "DNS name to access the service"
 }
 
 output "draft-router-api_internal_dns_name" {
-  value       = "${aws_route53_record.draft-router-api_internal_record.fqdn}"
+  value       = aws_route53_record.draft-router-api_internal_record.fqdn
   description = "DNS name to access draft-router-api"
 }
 
 output "external_service_dns_name" {
-  value       = "${join("", aws_route53_record.draft-cache_external_service_record.*.fqdn)}"
+  value       = join("", aws_route53_record.draft-cache_external_service_record.*.fqdn)
   description = "DNS name to access the external service"
 }
