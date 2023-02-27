5f8fd14455e0b0b3adaed57fb83d30ab992dbe59
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2023-02-27 16:09:19 +0000

    Add rate limit to Bouncer
    Bouncer accrues a lot of bots because of the number of domains it serves.
    
    It's only fair that we give it a bit of protection.
    
    The approach here is the same as the cache ALB:
    
      - grab the original IP from the true-client-ip header set by the CDN
      - return a 429 status and a Retry-After header
      - return a basic page for the unlikely human that trips the rate limit
    
    https://trello.com/c/z65OXqJ1/3099-bouncer-rate-limit-waf

--- terraform/projects/infra-public-wafs/bouncer_public_rule.tf
+++ terraform/projects/infra-public-wafs/bouncer_public_rule.tf
@@ -62,6 +62,78 @@ resource "aws_wafv2_web_acl" "bouncer_public" {
     }
   }
 
+  # set a base rate limit per IP looking back over the last 5 minutes
+  # this is checked every 30s
+  rule {
+    name     = "bouncer-public-base-rate-limit"
+    priority = 3
+
+    action {
+      block {
+        custom_response {
+          response_code = 429
+
+          response_header {
+            name  = "Retry-After"
+            value = 30
+          }
+
+          response_header {
+            name  = "Cache-Control"
+            value = "max-age=0, private"
+          }
+
+          custom_response_body_key = "bouncer-public-rule-429"
+        }
+      }
+    }
+
+    statement {
+      rate_based_statement {
+        limit              = var.bouncer_public_base_rate_limit
+        aggregate_key_type = "FORWARDED_IP"
+
+        forwarded_ip_config {
+          # We expect all requests to have this header set. As we're counting,
+          #it's a good chance to verify that by matching any that don't
+          fallback_behavior = "MATCH"
+          header_name       = "true-client-ip"
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "bouncer-public-base-rate-limit"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  custom_response_body {
+    key     = "bouncer-public-rule-429"
+    content = <<HTML
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>Welcome to GOV.UK</title>
+          <style>
+            body { font-family: Arial, sans-serif; margin: 0; }
+            header { background: black; }
+            h1 { color: white; font-size: 29px; margin: 0 auto; padding: 10px; max-width: 990px; }
+            p { color: black; margin: 30px auto; max-width: 990px; }
+          </style>
+        </head>
+        <body>
+          <header><h1>GOV.UK</h1></header>
+          <p>Sorry, there have been too many attempts to access this page.</p>
+          <p>Try again in a few minutes.</p>
+        </body>
+      </html>
+      HTML
+
+    content_type = "TEXT_HTML"
+  }
+
   visibility_config {
     cloudwatch_metrics_enabled = true
     metric_name                = "bouncer-public-web-acl"
