1fe6d6073591404ee7b73d8eb6627559568ab74b
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2023-05-10 16:27:58 +0100

    Update remote_state.tf generator to TF 0.12+

--- tools/generate-remote-state-boiler-plate.sh
+++ tools/generate-remote-state-boiler-plate.sh
@@ -19,42 +19,35 @@ EOH
 cat << 'EOT'
 
 variable "remote_state_bucket" {
-  type        = "string"
   description = "S3 bucket we store our terraform state in"
 }
 
 variable "remote_state_infra_vpc_key_stack" {
-  type        = "string"
   description = "Override infra_vpc remote state path"
   default     = ""
 }
 
 variable "remote_state_infra_networking_key_stack" {
-  type        = "string"
   description = "Override infra_networking remote state path"
   default     = ""
 }
 
 variable "remote_state_infra_security_groups_key_stack" {
-  type        = "string"
   description = "Override infra_security_groups stackname path to infra_vpc remote state "
   default     = ""
 }
 
 variable "remote_state_infra_root_dns_zones_key_stack" {
-  type        = "string"
   description = "Override stackname path to infra_root_dns_zones remote state "
   default     = ""
 }
 
 variable "remote_state_infra_stack_dns_zones_key_stack" {
-  type        = "string"
   description = "Override stackname path to infra_stack_dns_zones remote state "
   default     = ""
 }
 
 variable "remote_state_infra_monitoring_key_stack" {
-  type        = "string"
   description = "Override stackname path to infra_monitoring remote state "
   default     = ""
 }
@@ -65,60 +58,60 @@ variable "remote_state_infra_monitoring_key_stack" {
 data "terraform_remote_state" "infra_vpc" {
   backend = "s3"
 
-  config {
-    bucket = "${var.remote_state_bucket}"
+  config = {
+    bucket = var.remote_state_bucket
     key    = "${coalesce(var.remote_state_infra_vpc_key_stack, var.stackname)}/infra-vpc.tfstate"
-    region = "${var.aws_region}"
+    region = var.aws_region
   }
 }
 
 data "terraform_remote_state" "infra_networking" {
   backend = "s3"
 
-  config {
-    bucket = "${var.remote_state_bucket}"
+  config = {
+    bucket = var.remote_state_bucket
     key    = "${coalesce(var.remote_state_infra_networking_key_stack, var.stackname)}/infra-networking.tfstate"
-    region = "${var.aws_region}"
+    region = var.aws_region
   }
 }
 
 data "terraform_remote_state" "infra_security_groups" {
   backend = "s3"
 
-  config {
-    bucket = "${var.remote_state_bucket}"
+  config = {
+    bucket = var.remote_state_bucket
     key    = "${coalesce(var.remote_state_infra_security_groups_key_stack, var.stackname)}/infra-security-groups.tfstate"
-    region = "${var.aws_region}"
+    region = var.aws_region
   }
 }
 
 data "terraform_remote_state" "infra_root_dns_zones" {
   backend = "s3"
 
-  config {
-    bucket = "${var.remote_state_bucket}"
+  config = {
+    bucket = var.remote_state_bucket
     key    = "${coalesce(var.remote_state_infra_root_dns_zones_key_stack, var.stackname)}/infra-root-dns-zones.tfstate"
-    region = "${var.aws_region}"
+    region = var.aws_region
   }
 }
 
 data "terraform_remote_state" "infra_stack_dns_zones" {
   backend = "s3"
 
-  config {
-    bucket = "${var.remote_state_bucket}"
+  config = {
+    bucket = var.remote_state_bucket
     key    = "${coalesce(var.remote_state_infra_stack_dns_zones_key_stack, var.stackname)}/infra-stack-dns-zones.tfstate"
-    region = "${var.aws_region}"
+    region = var.aws_region
   }
 }
 
 data "terraform_remote_state" "infra_monitoring" {
   backend = "s3"
 
-  config {
-    bucket = "${var.remote_state_bucket}"
+  config = {
+    bucket = var.remote_state_bucket
     key    = "${coalesce(var.remote_state_infra_monitoring_key_stack, var.stackname)}/infra-monitoring.tfstate"
-    region = "${var.aws_region}"
+    region = var.aws_region
   }
 }
 EOT
