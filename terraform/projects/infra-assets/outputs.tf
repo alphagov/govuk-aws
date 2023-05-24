131928364f14adb8a539afac4937d73547337422
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2023-05-24 15:50:51 +0100

    Fix infra-assets to work with Terraform 0.12+.
    This module wasn't deployable previously. Now at least plans
    successfully.

---
+++ terraform/projects/infra-assets/outputs.tf
@@ -0,0 +1,3 @@
+output "asset_manager_bucket_arn" {
+  value = aws_s3_bucket.assets.arn
+}
---
+++ terraform/projects/infra-assets/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  default = "eu-west-1"
+}
+
+variable "aws_backup_region" {
+  default = "eu-west-2"
+}
+
+variable "aws_environment" {}
+
+variable "stackname" {
+  default = "govuk"
+}
+
