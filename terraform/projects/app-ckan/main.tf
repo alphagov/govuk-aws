20eb12a8fed8c61be3a82a9f61f73c32c4b14f59
kentsanggds <ken.tsang@digital.cabinet-office.gov.uk>
2023-07-14 16:52:51 +0100

    Add EKS security group to CKAN RDS sec group rule

--- terraform/projects/app-ckan/main.tf
+++ terraform/projects/app-ckan/main.tf
@@ -57,6 +57,11 @@ variable "instance_type" {
   default     = "m5.xlarge"
 }
 
+variable "eks_node_security_group" {
+  type        = "string"
+  description = "EKS node security group"
+}
+
 # Resources
 # --------------------------------------------------------------
 terraform {
@@ -276,6 +281,16 @@ resource "aws_security_group_rule" "ckan-rds_ingress_ckan_postgres" {
 
   security_group_id        = "${data.aws_security_group.ckan-rds.0.id}"
   source_security_group_id = "${data.terraform_remote_state.infra_security_groups.sg_ckan_id}"
+}
+
+resource "aws_security_group_rule" "ckan-rds_ingress_ckan_postgres" {
+  type      = "ingress"
+  from_port = 5432
+  to_port   = 5432
+  protocol  = "tcp"
+
+  security_group_id        = "${data.aws_security_group.ckan-rds.0.id}"
+  source_security_group_id = "${var.eks_node_security_group}"
 }
 
 # Outputs
