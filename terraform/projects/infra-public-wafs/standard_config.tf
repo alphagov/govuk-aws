0d2d9eee74e9266c5573f8f684e2287a76d2c1ae
Mahmud Hussain <mahmud.hussain@digital.cabinet-office.gov.uk>
2023-07-31 11:01:16 +0100

    Refactor aws_sheild_protection standard configs
    - Some of the apps have been migrated to Kubernetes, therefore, errored with
    'This object does not have an attribute'. This commit removes those attributes

--- terraform/projects/infra-public-wafs/standard_config.tf
+++ terraform/projects/infra-public-wafs/standard_config.tf
@@ -1,44 +1,4 @@
 
-resource "aws_shield_protection" "account_public_lb" {
-  name         = "${var.stackname}-account-public-lb_shield"
-  resource_arn = data.terraform_remote_state.infra_public_services.outputs.account_public_lb_id
-}
-
-resource "aws_wafv2_web_acl_association" "account_public_web_acl" {
-  resource_arn = data.terraform_remote_state.infra_public_services.outputs.account_public_lb_id
-  web_acl_arn  = aws_wafv2_web_acl.default.arn
-}
-
-resource "aws_shield_protection" "backend_public_lb" {
-  name         = "${var.stackname}-backend-public-lb_shield"
-  resource_arn = data.terraform_remote_state.infra_public_services.outputs.backend_public_lb_id
-}
-
-resource "aws_wafv2_web_acl_association" "backend_public_web_acl" {
-  resource_arn = data.terraform_remote_state.infra_public_services.outputs.backend_public_lb_id
-  web_acl_arn  = aws_wafv2_web_acl.backend_public.arn
-}
-
-resource "aws_shield_protection" "bouncer_public_lb" {
-  name         = "${var.stackname}-bouncer-public-lb_shield"
-  resource_arn = data.terraform_remote_state.infra_public_services.outputs.bouncer_public_lb_id
-}
-
-resource "aws_wafv2_web_acl_association" "bouncer_public_web_acl" {
-  resource_arn = data.terraform_remote_state.infra_public_services.outputs.bouncer_public_lb_id
-  web_acl_arn  = aws_wafv2_web_acl.bouncer_public.arn
-}
-
-resource "aws_wafv2_web_acl_association" "cache_public_web_acl" {
-  resource_arn = data.terraform_remote_state.infra_public_services.outputs.cache_public_lb_id
-  web_acl_arn  = aws_wafv2_web_acl.cache_public.arn
-}
-
-resource "aws_shield_protection" "cache_public_lb" {
-  name         = "${var.stackname}-cache-public-lb_shield"
-  resource_arn = data.terraform_remote_state.infra_public_services.outputs.cache_public_lb_id
-}
-
 resource "aws_shield_protection" "ckan_public_lb" {
   name         = "${var.stackname}-ckan-public-lb_shield"
   resource_arn = data.terraform_remote_state.infra_public_services.outputs.ckan_public_lb_id
@@ -59,25 +19,6 @@ resource "aws_wafv2_web_acl_association" "deploy_public_web_acl" {
   web_acl_arn  = aws_wafv2_web_acl.default.arn
 }
 
-resource "aws_shield_protection" "draft_cache_public_lb" {
-  name         = "${var.stackname}-draft-cache-public-lb_shield"
-  resource_arn = data.terraform_remote_state.infra_public_services.outputs.draft_cache_public_lb_id
-}
-
-resource "aws_wafv2_web_acl_association" "draft_cache_public_web_acl" {
-  resource_arn = data.terraform_remote_state.infra_public_services.outputs.draft_cache_public_lb_id
-  web_acl_arn  = aws_wafv2_web_acl.default.arn
-}
-
-resource "aws_shield_protection" "email_alert_api_public_lb" {
-  name         = "${var.stackname}-email-alert-api-public-lb_shield"
-  resource_arn = data.terraform_remote_state.infra_public_services.outputs.email_alert_api_public_lb_id
-}
-
-resource "aws_wafv2_web_acl_association" "email_alert_api_public_web_acl" {
-  resource_arn = data.terraform_remote_state.infra_public_services.outputs.email_alert_api_public_lb_id
-  web_acl_arn  = aws_wafv2_web_acl.default.arn
-}
 
 resource "aws_shield_protection" "graphite_public_lb" {
   name         = "${var.stackname}-graphite-public-lb_shield"
@@ -122,29 +63,4 @@ resource "aws_wafv2_web_acl_association" "monitoring_public_web_acl" {
 resource "aws_shield_protection" "prometheus_public_lb" {
   name         = "${var.stackname}-prometheus-public-lb_shield"
   resource_arn = data.terraform_remote_state.infra_public_services.outputs.prometheus_public_lb_id
-}
-
-resource "aws_wafv2_web_acl_association" "prometheus_public_web_acl" {
-  resource_arn = data.terraform_remote_state.infra_public_services.outputs.prometheus_public_lb_id
-  web_acl_arn  = aws_wafv2_web_acl.default.arn
-}
-
-resource "aws_shield_protection" "sidekiq_monitoring_public_lb" {
-  name         = "${var.stackname}-sidekiq-monitoring-public-lb_shield"
-  resource_arn = data.terraform_remote_state.infra_public_services.outputs.sidekiq_monitoring_public_lb_id
-}
-
-resource "aws_wafv2_web_acl_association" "sidekiq_monitoring_public_web_acl" {
-  resource_arn = data.terraform_remote_state.infra_public_services.outputs.sidekiq_monitoring_public_lb_id
-  web_acl_arn  = aws_wafv2_web_acl.default.arn
-}
-
-resource "aws_shield_protection" "whitehall_backend_public_lb" {
-  name         = "${var.stackname}-whitehall-backend-public-lb_shield"
-  resource_arn = data.terraform_remote_state.infra_public_services.outputs.whitehall_backend_public_lb_id
-}
-
-resource "aws_wafv2_web_acl_association" "whitehall_backend_public_web_acl" {
-  resource_arn = data.terraform_remote_state.infra_public_services.outputs.whitehall_backend_public_lb_id
-  web_acl_arn  = aws_wafv2_web_acl.default.arn
 }
