4357f2d981e7f59c9ba3dc7858196714b4851628
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2023-08-08 13:33:24 +0100

    Make app-elasticsearch6 module deployable again.
    - Update to a supported version of Terraform.
    - Update to the current version of the AWS TF provider.
    - Refactor to [standard module structure].
    - Use provider default tags.
    - Remove outdated comments.
    
    [standard module structure]: https://developer.hashicorp.com/terraform/language/modules/develop/structure

---
+++ terraform/projects/app-elasticsearch6/outputs.tf
@@ -0,0 +1,19 @@
+output "service_endpoint" {
+  value       = aws_elasticsearch_domain.elasticsearch6.endpoint
+  description = "Endpoint to submit index, search, and upload requests"
+}
+
+output "service_dns_name" {
+  value       = aws_route53_record.service_record.fqdn
+  description = "DNS name to access the Elasticsearch internal service"
+}
+
+output "domain_configuration_policy_arn" {
+  value       = aws_iam_policy.manual_snapshot_domain_configuration_policy.arn
+  description = "ARN of the policy used to configure the elasticsearch domain"
+}
+
+output "manual_snapshots_bucket_arn" {
+  value       = aws_s3_bucket.manual_snapshots.arn
+  description = "ARN of the bucket to store manual snapshots"
+}
