d946524846f238de5ec417a37f359c237a9ce519
kentsanggds <ken.tsang@digital.cabinet-office.gov.uk>
2023-09-27 15:57:46 +0100

    Remove ckan-rds

--- terraform/projects/app-ckan/main.tf
+++ terraform/projects/app-ckan/main.tf
@@ -25,17 +25,3 @@ provider "aws" {
   region  = "${var.aws_region}"
   version = "2.46.0"
 }
-
-data "aws_security_group" "ckan-rds" {
-  name = "${var.stackname}_ckan_rds_access"
-}
-
-resource "aws_security_group_rule" "ckan-rds_ingress_ckan_postgres" {
-  type      = "ingress"
-  from_port = 5432
-  to_port   = 5432
-  protocol  = "tcp"
-
-  security_group_id        = "${data.aws_security_group.ckan-rds.0.id}"
-  source_security_group_id = "${data.terraform_remote_state.infra_security_groups.sg_ckan_id}"
-}
