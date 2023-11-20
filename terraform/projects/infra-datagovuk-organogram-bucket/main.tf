af95e3bacea798d937e43e8968acd565b298e206
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2023-11-20 17:49:00 +0000

    Fix infra-datagovuk-organogram-bucket module.
    - Update to Terraform 1.x (via 0.13).
    - Update to current versions of providers.
    - Use provider-default tags for consistent AWS tagging.
    - Update deprecated syntax, resource config etc.
    
    Should now be deployable from arm64.
    
    Tested: applied in all three accounts, extra domains added to CORS config as expected.

--- terraform/projects/infra-datagovuk-organogram-bucket/main.tf
+++ terraform/projects/infra-datagovuk-organogram-bucket/main.tf
@@ -1,92 +1,71 @@
-/**
-* ## Project: datagovuk-organogram-bucket
-*
-* This creates an s3 bucket
-*
-* datagovuk-organogram-bucket: A bucket to hold data.gov.uk organogram files
-*
-*/
+// datagovuk-organogram-bucket defines an S3 bucket to hold data.gov.uk organogram files.
 
-variable "aws_region" {
-  type        = "string"
-  description = "AWS region"
-  default     = "eu-west-1"
-}
-
-variable "aws_environment" {
-  type        = "string"
-  description = "AWS Environment"
-}
-
-variable "domain" {
-  type        = "string"
-  description = "The domain of the data.gov.uk service to manage"
-}
-
-variable "stackname" {
-  type        = "string"
-  description = "Stackname"
-}
-
-variable "remote_state_bucket" {
-  type        = "string"
-  description = "S3 bucket we store our terraform state in"
-}
-
-variable "remote_state_infra_monitoring_key_stack" {
-  type        = "string"
-  description = "Override stackname path to infra_monitoring remote state "
-  default     = ""
-}
-
-# Set up the backend & provider for each region
 terraform {
   backend "s3" {}
-  required_version = "~> 0.12.31"
+  required_version = "~> 1.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    fastly = {
+      source  = "fastly/fastly"
+      version = "~> 5.0"
+    }
+  }
 }
 
 provider "aws" {
-  region  = "${var.aws_region}"
-  version = "2.46.0"
+  region = var.aws_region
+  default_tags {
+    tags = {
+      terraform_deployment = basename(abspath(path.root))
+      aws_environment      = var.aws_environment
+    }
+  }
 }
+
+provider "fastly" { api_key = "test" }
 
 data "terraform_remote_state" "infra_monitoring" {
   backend = "s3"
 
   config = {
-    bucket = "${var.remote_state_bucket}"
-    key    = "${coalesce(var.remote_state_infra_monitoring_key_stack, var.stackname)}/infra-monitoring.tfstate"
-    region = "${var.aws_region}"
+    bucket = var.remote_state_bucket
+    key    = "govuk/infra-monitoring.tfstate"
+    region = var.aws_region
   }
 }
 
 resource "aws_s3_bucket" "datagovuk-organogram" {
   bucket = "datagovuk-${var.aws_environment}-ckan-organogram"
+  tags   = { Name = "datagovuk-${var.aws_environment}-ckan-organogram" }
+}
 
-  dynamic "cors_rule" {
-    for_each = [for s in ["${var.domain}", "https://staging.data.gov.uk", "https://find.eks.${var.aws_environment}.govuk.digital"] : {
-      allowed_origin = s
-    }]
+resource "aws_s3_bucket_versioning" "datagovuk_organogram" {
+  bucket = aws_s3_bucket.datagovuk-organogram.id
+  versioning_configuration { status = "Enabled" }
+}
+
+resource "aws_s3_bucket_logging" "datagovuk_organogram" {
+  bucket        = aws_s3_bucket.datagovuk-organogram.id
+  target_bucket = data.terraform_remote_state.infra_monitoring.outputs.aws_logging_bucket_id
+  target_prefix = "s3/datagovuk-${var.aws_environment}-ckan-organogram/"
+}
+
+resource "aws_s3_bucket_cors_configuration" "datagovuk_organogram" {
+  bucket = aws_s3_bucket.datagovuk-organogram.id
+  cors_rule {
     allowed_methods = ["GET"]
-    allowed_origins = [cors_rule.value.allowed_origin]
-  }
-
-  tags = {
-    Name            = "datagovuk-${var.aws_environment}-ckan-organogram"
-    aws_environment = "${var.aws_environment}"
-  }
-
-  logging {
-    target_bucket = "${data.terraform_remote_state.infra_monitoring.outputs.aws_logging_bucket_id}"
-    target_prefix = "s3/datagovuk-${var.aws_environment}-ckan-organogram/"
-  }
-
-  versioning {
-    enabled = true
+    allowed_origins = [
+      var.domain,
+      "https://staging.data.gov.uk",
+      "https://find.eks.${var.aws_environment}.govuk.digital"
+    ]
   }
 }
 
 resource "aws_s3_bucket_policy" "govuk_datagovuk_organogram_read_policy" {
-  bucket = "${aws_s3_bucket.datagovuk-organogram.id}"
-  policy = "${data.aws_iam_policy_document.s3_fastly_read_policy_doc.json}"
+  bucket = aws_s3_bucket.datagovuk-organogram.id
+  policy = data.aws_iam_policy_document.s3_fastly_read_policy_doc.json
 }
