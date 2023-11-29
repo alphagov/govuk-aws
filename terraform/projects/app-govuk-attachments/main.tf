89375dbc17479fec0d13af81be1d687ce63d2dc3
martynipratt <martyn.pratt@digital.cabinet-office.gov.uk>
2023-11-29 11:44:40 +0000

    app-govuk-attachments

--- terraform/projects/app-govuk-attachments/main.tf
+++ terraform/projects/app-govuk-attachments/main.tf
@@ -70,6 +70,7 @@ resource "aws_s3_bucket" "bucket" {
   acl    = "private"
 
   tags {
+    Name          = "govuk-${var.env}-${var.region}-attachments"
     Environment = var.aws_environment
     Team        = var.team
     Product     = "GOVUK"
@@ -98,6 +99,7 @@ resource "aws_s3_bucket" "bucket_without_lifecycle" {
   acl    = "private"
 
   tags {
+    Name          = "govuk-${var.env}-${var.region}-attachments"
     Environment = var.aws_environment
     Team        = var.team
     Owner       = "govuk-replatforming-team@digital.cabinet-office.gov.uk"
