004099553e01e3c7d27908b471a9accee62b7f44
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2023-12-14 15:08:44 +0000

    Remove defunct WAF association objects for CKAN.
    These were referring to LBs that no longer exist, and their replacements
    are configured via aws-lb-controller and not Terraform.

--- terraform/projects/infra-public-wafs/standard_config.tf
+++ terraform/projects/infra-public-wafs/standard_config.tf
@@ -1,14 +1,4 @@
 
-resource "aws_shield_protection" "ckan_public_lb" {
-  name         = "${var.stackname}-ckan-public-lb_shield"
-  resource_arn = data.terraform_remote_state.infra_public_services.outputs.ckan_public_lb_id
-}
-
-resource "aws_wafv2_web_acl_association" "ckan_public_web_acl" {
-  resource_arn = data.terraform_remote_state.infra_public_services.outputs.ckan_public_lb_id
-  web_acl_arn  = aws_wafv2_web_acl.default.arn
-}
-
 resource "aws_shield_protection" "deploy_public_lb" {
   name         = "${var.stackname}-deploy-public-lb_shield"
   resource_arn = data.terraform_remote_state.infra_public_services.outputs.deploy_public_lb_id
