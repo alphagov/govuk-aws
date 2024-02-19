11d494e8ca8b9811e9d7eb11bd67badb8b11afc9
Sam Simpson <sam.simpson@digital.cabinet-office.gov.uk>
2024-02-19 13:33:39 +0000

    Remove app-govuk-rds project
    Project has moved to govuk-infrastructure

--- terraform/projects/app-govuk-rds/security_groups.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_security_group" "rds" {
-  for_each = var.databases
-
-  name        = "${var.stackname}_${each.value.name}_rds_access"
-  vpc_id      = data.terraform_remote_state.infra_vpc.outputs.vpc_id
-  description = "Access to ${each.value.name} RDS"
-  tags        = { Name = "${var.stackname}_${each.value.name}_rds_access" }
-}
