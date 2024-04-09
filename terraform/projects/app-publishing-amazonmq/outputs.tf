60917714286eda71ec88d3bc3966268d8b2e29d9
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2024-04-09 14:40:06 +0100

    Clean up publishing-amazonmq ready for moving.
    - Avoid relying on a DNS query that only works when running Terraform
      from inside the VPC.
    - Remove references to the deleted `govuk_management_access` security
      group.
    - Update TF and provider versions.
    - Remove disused variables.
    - Fix lint errors.
    - Strip redundant comments.
    - Fix AWS tagging.
    
    Tested: applied in staging.

--- terraform/projects/app-publishing-amazonmq/outputs.tf
+++ terraform/projects/app-publishing-amazonmq/outputs.tf
@@ -5,12 +5,12 @@ output "broker_id" {
 
 output "console_url" {
   description = "URL of the RabbitMQ web management UI"
-  value       = aws_mq_broker.publishing_amazonmq.instances.0.console_url
+  value       = aws_mq_broker.publishing_amazonmq.instances[0].console_url
 }
 
 output "amqp_endpoint" {
   description = "AMQP URL for connecting a client app to the broker"
-  value       = aws_mq_broker.publishing_amazonmq.instances.0.endpoints.0
+  value       = aws_mq_broker.publishing_amazonmq.instances[0].endpoints[0]
 }
 
 output "internal_domain_name" {
@@ -18,9 +18,7 @@ output "internal_domain_name" {
   value       = aws_route53_record.publishing_amazonmq_internal_root_domain_name.fqdn
 }
 
-#----------------------------------------------------------------------
-# These outputs are marked sensitive, so will not be shown in STDOUT 
-# by default. To retrieve them, use `terraform output -json`
+# Use `terraform output -json` to retrieve these sensitive=true outputs.
 output "publishing_amazonmq_passwords" {
   description = "Generated passwords for each RabbitMQ user account. Use `terraform output -json | jq '.publishing_amazonmq_passwords.value'` to retrieve the values."
   sensitive   = true
