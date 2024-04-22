af8426eb7c603483e38868eb8aca2e6a6cc172a3
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2024-04-22 17:44:18 +0100

    Auto-format post_config_to_amazonmq.py + fix lints.

--- terraform/lambda/PostConfigToAmazonMQ/post_config_to_amazonmq.py
+++ terraform/lambda/PostConfigToAmazonMQ/post_config_to_amazonmq.py
@@ -5,30 +5,34 @@ import urllib.error
 
 from urllib import request
 
+
 def raise_if_not_an_amazonmq_url(url):
     if re.fullmatch(r'.*\.mq\.[a-z0-9_-]+\.amazonaws\.com/.*', url) is None:
         raise ValueError(f'Not a valid AmazonMQ URL: {url}')
 
+
 def auth_digest(username, password):
-    credentials = "%s:%s" % (username, password)
+    credentials = f'{username}:{password}'
     return base64.b64encode(credentials.encode('UTF8'))
 
+
 def add_headers(req, event):
-    auth_header_value = b'Basic ' + auth_digest(event['username'], event['password'])
+    auth_header_value = b'Basic ' + \
+        auth_digest(event['username'], event['password'])
     req.add_header('Authorization', auth_header_value)
-
     req.add_header('Content-type', 'application/json')
-
     return req
 
-def lambda_handler(event, context):
+
+def lambda_handler(event, _):
     raise_if_not_an_amazonmq_url(event['url'])
 
     raw_json = base64.b64decode(event['json_b64'])
-    req = request.Request(event['url'], data=raw_json, method='POST', unverifiable=True)
+    req = request.Request(event['url'], data=raw_json,
+                          method='POST', unverifiable=True)
     add_headers(req, event)
 
-    print('Posting json to ', event['url'])
+    print('Posting JSON to ', event['url'])
     try:
         resp = request.urlopen(req)
         status = resp.status
